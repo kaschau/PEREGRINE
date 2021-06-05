92bbea1bea2444af0171c9d3f51a89036d98b833
Kyle Schau <kschau@gatech.edu>
2021-06-05 11:43:14 -0400

    Went to 3D, trying new stuff

--- Src/perepute.cpp
+++ Src/perepute.cpp
@@ -1,16 +1,5 @@
 #include "user.hpp"
-
-//#if defined(__GNUC__)
-//#  pragma GCC diagnostic push
-//#  pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-//#endif
-
 #include <pybind11/pybind11.h>
-
-//#if defined(__GNUC__)
-//#  pragma GCC diagnostic pop
-//#endif
-
 #include <cstdlib>
 
 namespace py = pybind11;
@@ -27,11 +16,21 @@ PYBIND11_MODULE(perepute, m) {
   /// declared in user.hpp which returns a Kokkos::View. This function is called
   /// from ex-numpy.py
   ///
-  m.def("generate_view", &generate_view, "Generate a random view",
-         py::arg("n") = 10);
+  //m.def("generate_view", &generate_view, "Generate a random view",
+  //       py::arg("n") = 10);
 
-  m.def("generate_view2", &generate_view2, "Generate a random view",
-         py::arg("n") = 15);
+  m.def("add", &add, "Add a float to entire view",
+         py::arg("view"), py::arg("adder"));
+  m.def("add2", &add2, "Add a float to entire view",
+         py::arg("view"), py::arg("adder"),
+         py::arg("imin"), py::arg("jmin"), py::arg("kmin"),
+         py::arg("imax"), py::arg("jmax"), py::arg("kmax"));
+  m.def("add3", &add3, "Add a float to entire view",
+         py::arg("view"), py::arg("adder"),
+         py::arg("imin"),
+         py::arg("jmin"),
+         py::arg("imax"),
+         py::arg("imax")) ;
 
   static auto _atexit = []() {
     if (Kokkos::is_initialized()) Kokkos::finalize();
--- Src/user.cpp
+++ Src/user.cpp
@@ -1,43 +1,65 @@
-
 #include "user.hpp"
-
 #include "Kokkos_Core.hpp"
-
 #include <cstdint>
+#include <iostream>
 
-struct InitView {
-  explicit InitView(view_type _v) : m_view(_v) {}
-
-  KOKKOS_INLINE_FUNCTION
-  void operator()(const int i) const { m_view(i, i % 2) = i; }
-
- private:
-  view_type m_view;
-};
+//struct InitView {
+//  explicit InitView(view_type _v) : m_view(_v) {}
+//
+//  KOKKOS_INLINE_FUNCTION
+//  void operator()(const int i) const { m_view(i, i % 2) = i; }
+//
+// private:
+//  view_type m_view;
+//};
 
 ///
 /// \fn generate_view
 /// \brief This is meant to emulate some function that exists in a user library
 /// which returns a Kokkos::View and will have a python binding
 ///
-view_type generate_view(size_t n) {
-  if (!Kokkos::is_initialized()) {
-    std::cerr << "[user-bindings]> Initializing Kokkos..." << std::endl;
-    Kokkos::initialize();
-  }
-  view_type _v("user_view", n, 2);
-  Kokkos::RangePolicy<exec_space, int> range(0, n);
-  Kokkos::parallel_for("generate_view", range, InitView{_v});
-  return _v;
+//view_type generate_view(size_t n) {
+  //if (!Kokkos::is_initialized()) {
+    //std::cerr << "[user-bindings]> Initializing Kokkos..." << std::endl;
+    //Kokkos::initialize();
+  //}
+  //view_type _v("user_view", n, 2);
+  //Kokkos::RangePolicy<exec_space, int> range(0, n);
+  //Kokkos::parallel_for("generate_view", range, InitView{_v});
+  //return _v;
+//}
+
+void add(oneDview kkview, double n) {
+
+  auto len = kkview.size();
+  std::cout << kkview.size() << std::endl;
+  Kokkos::parallel_for("add", len, KOKKOS_LAMBDA(const int i) {
+    kkview(i) += n;
+  });
 }
 
-view_type generate_view2(size_t n) {
-  if (!Kokkos::is_initialized()) {
-    std::cerr << "[user-bindings]> Initializing Kokkos..." << std::endl;
-    Kokkos::initialize();
-  }
-  view_type _v("user_view", n, 2);
-  Kokkos::RangePolicy<exec_space, int> range(0, n);
-  Kokkos::parallel_for("generate_view2", range, InitView{_v});
-  return _v;
+void add2(threeDview kkview, double n,
+          int imin, int jmin, int kmin,
+          int imax, int jmax, int kmax) {
+
+  MDRange3 _range({{imin,jmin,kmin}},{{imax,jmax,kmax}});
+  Kokkos::parallel_for("add2", _range, KOKKOS_LAMBDA(const int i, const int j, const int k) {
+      kkview(i,j,k) += n - 1.0;
+      kkview(i,j,k) += n + 1.0*2.0;
+      kkview(i,j,k) += n / 1.0;
+      kkview(i,j,k) += n - 1.0;
+      kkview(i,j,k) += n;
+      kkview(i,j,k) += n;
+      kkview(i,j,k) += n;
+      kkview(i,j,k) += n;
+      kkview(i,j,k) += n;
+  });
+}
+
+void add3(twoDview kkview, double n, int imin, int jmin, int imax, int jmax) {
+
+  for (int j=jmin; j<jmax; j++) {
+  for (int i=imin; i<imax; i++) {
+    kkview(i,j) += n;
+  }};
 }
