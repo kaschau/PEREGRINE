991233f74729b60231925cfb7fee0c35dd5da4b1
Kyle A. Schau <kaschau89@gmail.com>
2021-06-05 11:43:14 -0400

    Went to 3D, trying new stuff

---
+++ Src/user.hpp
@@ -0,0 +1,29 @@
+#include "Kokkos_Core.hpp"
+#include <cstdint>
+
+#if defined(KOKKOS_ENABLE_CUDA_UVM)
+using exec_space = Kokkos::Cuda;
+using view_space = Kokkos::CudaUVMSpace;
+#elif defined(KOKKOS_ENABLE_CUDA)
+using exec_space = Kokkos::Cuda;
+using view_space = Kokkos::CudaSpace;
+#elif defined(KOKKOS_ENABLE_HIP)
+using exec_space = Kokkos::Experimental::HIP;
+using view_space = Kokkos::Experimental::HIPSpace;
+#else
+using exec_space = Kokkos::DefaultHostExecutionSpace;
+//using exec_space = Kokkos::Serial;
+using view_space = Kokkos::HostSpace;
+#endif
+
+using oneDview   = Kokkos::View<double*,    Kokkos::LayoutRight, view_space>;
+using twoDview   = Kokkos::View<double**,   Kokkos::LayoutRight, view_space>;
+using threeDview = Kokkos::View<double***,  Kokkos::LayoutRight, view_space>;
+using fourDview  = Kokkos::View<double****, Kokkos::LayoutRight, view_space>;
+using MDRange2   = Kokkos::MDRangePolicy<exec_space,Kokkos::Rank<2>>;
+using MDRange3   = Kokkos::MDRangePolicy<exec_space,Kokkos::Rank<3>>;
+
+void add(oneDview kkview, double n);
+void add2(threeDview kkview, double n, int imin, int jmin, int kmin,
+                                       int imax, int jmax, int kmax);
+void add3(twoDview kkview, double n, int imin, int jmin, int imax, int jmax);
--- peregrine.py
+++ peregrine.py
@@ -6,41 +6,21 @@ import numpy as np
 # The declaration and definition of generate_view are in user.hpp and user.cpp
 # The generate_view function will return a Kokkos::View and will be converted
 # to a numpy array
-from perepute import generate_view
+from perepute import add2,add3
 
 # Importing this module is necessary to call kokkos init/finalize and
 # import the python bindings to Kokkos::View which generate_view will
 # return
 #
 import kokkos
-
-
-#def main(args):
-#    # get the kokkos view
-#    view = generate_view2()
-#    # verify the type id
-#    print("Kokkos View : {} (shape={})".format(type(view).__name__, view.shape))
-#    # print data provided by generate_view
-#    if view.space != kokkos.CudaSpace:
-#        for i in range(view.shape[0]):
-#            print(
-#                "    view({}) = [{:1.0f}., {:1.0f}.]".format(i, view[i, 0], view[i, 1])
-#            )
-#    # wrap the buffer protocal as numpy array without copying the data
-#    arr = np.array(view, copy=False)
-#    # verify type id
-#    print("Numpy Array : {} (shape={})".format(type(arr).__name__, arr.shape))
-#    # demonstrate the data is the same as what was printed by generate_view
-#    if view.space != kokkos.CudaSpace:
-#        for i in range(arr.shape[0]):
-#            print("     arr({}) = {}".format(i, arr[i]))
-
+import time
 
 def test():
+    nx,ny,nz = 1000,1000,1000
     # get the kokkos view
     view = kokkos.array(
         "python_allocated_view",
-        [10,10],
+        [nx,ny,nz],
         dtype=kokkos.double,
         space=kokkos.HostSpace,
     )
@@ -53,8 +33,14 @@ def test():
     # verify type id
     # print("Numpy Array : {} (shape={})".format(type(arr).__name__, arr.shape))
     # demonstrate the data is the same as what was printed by generate_view
-    print(view.space)
-
+    #print(arr)
+    ts = time.time()
+    add2(view,1.0,0,0,0,nx,ny,nz)
+    add2(view,1.0,0,0,0,nx,ny,nz)
+    add2(view,1.0,0,0,0,nx,ny,nz)
+    add2(view,1.0,0,0,0,nx,ny,nz)
+    print(time.time()-ts, 'took this many seconds')
+    #print(arr)
 
 if __name__ == "__main__":
     kokkos.initialize()
