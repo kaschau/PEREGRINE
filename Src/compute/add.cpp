69d54c71c147d1ae67b9e5abacacbc950bf79e55
Kyle Schau <kschau@gatech.edu>
2021-06-05 22:02:28 -0400

    Added a grid reader to peregrinpy. Changed nx,ny,nz to ni,nj.nk

--- Src/compute/add.cpp
+++ Src/compute/add.cpp
@@ -6,7 +6,7 @@
 
 void add3(block b, double n ) {
 
-  MDRange3 _range({{0,0,0}},{{b.nx,b.ny,b.nz}});
+  MDRange3 _range({{0,0,0}},{{b.ni,b.nj,b.nk}});
   Kokkos::parallel_for("add3", _range, KOKKOS_LAMBDA(const int i, const int j, const int k) {
 
       b.x(i,j,k) += n;
---
+++ peregrinepy/readers/__init__.py
@@ -0,0 +1,2 @@
+from .read_blocks4procs import read_blocks4procs
+from .read_grid import read_grid
