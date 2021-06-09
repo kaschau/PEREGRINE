4adc7d80cb96e448ce82765d667f93bd1b738a72
Kyle A. Schau <kaschau89@gmail.com>
2021-06-09 12:52:33 -0400

    idk why im pushing this

--- src/peregrinepy/mpicomm/halo_grid.py
+++ src/peregrinepy/mpicomm/halo_grid.py
@@ -1,15 +1,15 @@
 from .mpiutils import get_comm_rank_size
 
-import numpy as np
-
 
 def halo_grid(mb,config):
 
     comm,rank,size = get_comm_rank_size()
     block_list = mb.block_list
     for blk in mb:
-        for face in [str(i+1) for i in range(6)]:
+        for face in ['1','2','3','4','5','6']:
             neighbor    = blk.connectivity[face]['neighbor']
+            if neighbor is None:
+                continue
             orientation = blk.connectivity[face]['orientation']
             comm_rank   = blk.connectivity[face]['comm_rank']
             #if neighbor
