02184d6378755ed5dacd1d3f3b20984a8e8dea42
Kyle A. Schau <kaschau89@gmail.com>
2021-06-10 09:42:16 -0400

    Added testing structure

---
+++ tests/MPI/test_orientation.py
@@ -0,0 +1,10 @@
+import unittest
+import peregrinepy as pg
+
+class TestOrientation(unittest.TestCase):
+    def test_orientation(self):
+        self.assertEqual(2, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
