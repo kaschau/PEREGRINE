6235e6e6f7b1a4632aeb14556b94de104d65e0a5
Kyle A. Schau <kaschau89@gmail.com>
2021-08-02 19:06:55 -0400

    Unit test comparing cpg module to cantera results.

--- tests/thermo/test_cpg.py
+++ tests/thermo/test_cpg.py
@@ -1,13 +1,9 @@
-import mpi4py.rc
-mpi4py.rc.initialize = False
-
-import kokkos
-
 import peregrinepy as pg
 import numpy as np
 import cantera as ct
 
 import sys
+from pathlib import Path
 
 #np.random.seed(111)
 
@@ -15,11 +11,12 @@ import sys
 ##### Test for all positive i aligned orientations
 ##################################################################################
 
-def test_thermo(ctfile):
+def test_cpg():
+    import kokkos
+    kokkos.initialize()
 
-    # Import but do not initialise MPI
-    from mpi4py import MPI
-
+    relpath = str(Path(__file__).parent)
+    ctfile = relpath+'/dummy_ct.yaml'
     gas = ct.Solution(ctfile)
     p = np.random.uniform(low=10000, high=100000)
     T = np.random.uniform(low=100  , high=1000)
@@ -27,12 +24,6 @@ def test_thermo(ctfile):
     Y = Y/np.sum(Y)
 
     gas.TPY = T,p,Y
-
-    # Manually initialise MPI
-    MPI.Init()
-    comm,rank,size = pg.mpicomm.mpiutils.get_comm_rank_size()
-    # Ensure MPI is suitably cleaned up
-    pg.mpicomm.mpiutils.register_finalize_handler()
 
     config = pg.files.config_file()
     config['thermochem']['ctfile'] = ctfile
@@ -49,8 +40,6 @@ def test_thermo(ctfile):
 
     pg.grid.generate_halo(mb,config)
 
-    pg.mpicomm.blockcomm.set_block_communication(mb,config)
-
     pg.compute.metrics(mb)
 
     blk.array['q'][:,:,:,0] = p
@@ -64,32 +53,57 @@ def test_thermo(ctfile):
     pgcons = blk.array['Q'][1,1,1]
     pgprim = blk.array['q'][1,1,1]
     pgthrm = blk.array['qh'][1,1,1]
-    print(f'    {"Cantera":<11}  | {"PEREGRINE":<11}')
-    print(f'p:  {gas.P:10.5f}  | {pgprim[0]:10.5f}')
-    print(f'T:  {gas.T:10.5f}  | {pgprim[4]:10.5f}')
+
+
+    def print_diff(name,c,p):
+        diff = np.abs(c-p)/p*100
+        print(f'{name:<6s}: {c:16.8f} | {p:16.8f} | {diff:4.2f}')
+
+        return diff
+
+    pd = []
+    print('******** Primatives to Conservatives ***************')
+    print(f'       {"Cantera":<16}  | {"PEREGRINE":<16} | {"%Error":<6}')
+    print('Primatives')
+    pd.append(print_diff('p',gas.P,pgprim[0]))
+    pd.append(print_diff('T',gas.T,pgprim[4]))
     for i,n in enumerate(gas.species_names[0:-1]):
-        print(f'{n}:   {gas.Y[i]:>10.5f}  | {pgprim[5+i]:>10.5f}')
-    print(f'{gas.species_names[-1]}:   {gas.Y[-1]:>10.5f}  | {1.0-np.sum(pgprim[5::]):>10.5f}')
-    print(f'rho:{gas.density:>10.5f}  | {pgcons[0]:>10.5f}')
-    print(f'E:  {gas.int_energy_mass:>10.5f}  | {pgcons[4]/pgcons[0]:>10.5f}')
-    print(f'gamma:{gas.cp/gas.cv:>10.5f}  | {pgthrm[0]:>10.5f}')
-    print(f'cp:{gas.cp:>10.5f}  | {pgthrm[1]:>10.5f}')
-    print(f'h:{gas.enthalpy_mass:>10.5f}  | {pgthrm[2]/pgcons[0]:>10.5f}')
+        pd.append(print_diff(n,gas.Y[i],pgprim[5+i]))
+    pd.append(print_diff(gas.species_names[-1],gas.Y[-1],1.0-np.sum(pgprim[5::])))
+    print('Conservatives')
+    pd.append(print_diff('rho', gas.density,pgcons[0]))
+    pd.append(print_diff('E', gas.int_energy_mass, pgcons[4]/pgcons[0]))
+    for i,n in enumerate(gas.species_names[0:-1]):
+        pd.append(print_diff('rho'+n,gas.Y[i]*gas.density,pgcons[5+i]))
+    pd.append(print_diff('rho'+gas.species_names[-1],gas.Y[-1]*gas.density,pgcons[0]-np.sum(pgcons[5::])))
+    print('Mixture Properties')
+    pd.append(print_diff('gamma', gas.cp/gas.cv, pgthrm[0]))
+    pd.append(print_diff('cp', gas.cp, pgthrm[1]))
+    pd.append(print_diff('h', gas.enthalpy_mass, pgthrm[2]/pgcons[0]))
 
-    # Finalise MPI
-    MPI.Finalize()
+    #Go the other way
+    pg.consistify(mb)
 
-if __name__ == "__main__":
-    try:
-        kokkos.initialize()
-        test_thermo('dummy_ct.yaml')
-        kokkos.finalize()
+    print('********  Conservatives to Primatives ***************')
+    print(f'       {"Cantera":<15}  | {"PEREGRINE":<15} | {"%Error":<5}')
+    print('Conservatives')
+    pd.append(print_diff('rho', gas.density,pgcons[0]))
+    pd.append(print_diff('E', gas.int_energy_mass, pgcons[4]/pgcons[0]))
+    for i,n in enumerate(gas.species_names[0:-1]):
+        pd.append(print_diff('rho'+n,gas.Y[i]*gas.density,pgcons[5+i]))
+    pd.append(print_diff('rho'+gas.species_names[-1],gas.Y[-1]*gas.density,pgcons[0]-np.sum(pgcons[5::])))
+    print('Primatives')
+    pd.append(print_diff('p',gas.P,pgprim[0]))
+    pd.append(print_diff('T',gas.T,pgprim[4]))
+    for i,n in enumerate(gas.species_names[0:-1]):
+        pd.append(print_diff(n,gas.Y[i],pgprim[5+i]))
+    pd.append(print_diff(gas.species_names[-1],gas.Y[-1],1.0-np.sum(pgprim[5::])))
+    print('Mixture Properties')
+    pd.append(print_diff('gamma', gas.cp/gas.cv, pgthrm[0]))
+    pd.append(print_diff('cp', gas.cp, pgthrm[1]))
+    pd.append(print_diff('h', gas.enthalpy_mass, pgthrm[2]/pgcons[0]))
 
-    except Exception as e:
-        import sys
-        import traceback
+    kokkos.finalize()
 
-        print(f"{e}")
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback)
-        sys.exit(1)
+    passfail = np.all(np.array(pd) < 0.0001)
+    assert passfail
