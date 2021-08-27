790858945a3f49d966afb217d2dffbb2ce771c72
Kyle A. Schau <kaschau89@gmail.com>
2021-08-27 13:40:59 -0400

    Lots of stuff. Moved metrics out of C++ and into python so
    we have easier access them with grid/restart objects. Fixes #18
    
    Found a bug in the periodic communication that was terrible, Fixes #22

--- src/peregrinepy/multiblock/grid_block.py
+++ src/peregrinepy/multiblock/grid_block.py
@@ -13,6 +13,8 @@ This module defines a grid_block object that is used to compose a raptorpy.multi
 import numpy as np
 from .topology_block import topology_block
 from ..misc import FrozenDict
+from ..grid import metrics
+from ..grid import generate_halo
 
 
 class grid_block(topology_block):
@@ -62,9 +64,36 @@ class grid_block(topology_block):
         Create zeroed numpy arrays of correct size.
         '''
 
-        if self.block_type == 'solver':
-            xshape  = [self.ni+2,self.nj+2,self.nk+2]
-        else:
-            xshape  = [self.ni,self.nj,self.nk]
+        # Primary grid coordinates
+        shape = [self.ni+2,self.nj+2,self.nk+2] if self.block_type == 'solver' else [self.ni,self.nj,self.nk]
         for name in ['x','y','z']:
-            self.array[name] = np.zeros((xshape))
+            self.array[name] = np.zeros((shape))
+
+        # Cell center locations, volumes, diffusive metrics
+        shape = [self.ni+1,self.nj+1,self.nk+1] if self.block_type == 'solver' else [self.ni-1,self.nj-1,self.nk-1]
+        for name in ['xc','yc','zc','J',
+                     'dEdx','dEdy','dEdz',
+                     'dNdx','dNdy','dNdz',
+                     'dXdx','dXdy','dXdz']:
+            self.array[name] = np.zeros((shape))
+
+        # i face normal, area vectors
+        shape = [self.ni+2,self.nj+1,self.nk+1] if self.block_type == 'solver' else [self.ni,self.nj-1,self.nk-1]
+        for name in ['isx','isy','isz','iS','inx','iny','inz']:
+            self.array[name] = np.zeros((shape))
+
+        # j face normal, area vectors
+        shape = [self.ni+1,self.nj+2,self.nk+1] if self.block_type == 'solver' else [self.ni-1,self.nj,self.nk-1]
+        for name in ['jsx','jsy','jsz','jS','jnx','jny','jnz']:
+            self.array[name] = np.zeros((shape))
+
+        # k face normal, area vectors
+        shape = [self.ni+1,self.nj+1,self.nk+2] if self.block_type == 'solver' else [self.ni-1,self.nj-1,self.nk]
+        for name in ['ksx','ksy','ksz','kS','knx','kny','knz']:
+            self.array[name] = np.zeros((shape))
+
+    def compute_metrics(self):
+        metrics(self)
+
+    def generate_halo(self):
+        generate_halo(self)
