516564b16e6a177ef2cb6b7bce61e4e3269e61f4
Kyle Schau <kschau@gatech.edu>
2021-08-31 21:30:08 -0400

    Just aligning things.

--- src/compute/flux/advective.cpp
+++ src/compute/flux/advective.cpp
@@ -36,16 +36,16 @@ for(block_ b : mb){
     b.iF(i,j,k,0) = rho * U;
 
     // x momentum rho*u*Ui+ p*Ax
-    b.iF(i,j,k,1) =                               rho * 0.5*(b.q(i,j,k,1)+b.q(i-1,j,k,1)) * U
-                  + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) *      b.isx(i,j,k)                   ;
+    b.iF(i,j,k,1) = rho * 0.5*(b.q(i,j,k,1)+b.q(i-1,j,k,1)) * U
+                        + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) * b.isx(i,j,k) ;
 
     // y momentum rho*v*Ui+ p*Ay
-    b.iF(i,j,k,2) =                               rho * 0.5*(b.q(i,j,k,2)+b.q(i-1,j,k,2)) * U
-                  + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) *      b.isy(i,j,k)                   ;
+    b.iF(i,j,k,2) = rho * 0.5*(b.q(i,j,k,2)+b.q(i-1,j,k,2)) * U
+                        + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) * b.isy(i,j,k) ;
 
     // w momentum rho*w*Ui+ p*Az
-    b.iF(i,j,k,3) =                               rho * 0.5*(b.q(i,j,k,3)+b.q(i-1,j,k,3)) * U
-                  + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) *      b.isz(i,j,k)                   ;
+    b.iF(i,j,k,3) = rho * 0.5*(b.q(i,j,k,3)+b.q(i-1,j,k,3)) * U
+                        + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) * b.isz(i,j,k) ;
 
     // Total energy (rhoE+ p)*Ui)
     double e;
@@ -109,16 +109,16 @@ for(block_ b : mb){
     b.jF(i,j,k,0) = rho * V;
 
     // x momentum rho*u*Vj+ pAx
-    b.jF(i,j,k,1) =                               rho * 0.5*(b.q(i,j,k,1)+b.q(i,j-1,k,1)) * V
-                  + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) *      b.jsx(i,j,k)                   ;
+    b.jF(i,j,k,1) = rho * 0.5*(b.q(i,j,k,1)+b.q(i,j-1,k,1)) * V
+                        + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) * b.jsx(i,j,k) ;
 
     // y momentum rho*v*Vj+ pAy
-    b.jF(i,j,k,2) =                               rho * 0.5*(b.q(i,j,k,2)+b.q(i,j-1,k,2)) * V
-                  + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) *      b.jsy(i,j,k)                   ;
+    b.jF(i,j,k,2) = rho * 0.5*(b.q(i,j,k,2)+b.q(i,j-1,k,2)) * V
+                        + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) * b.jsy(i,j,k) ;
 
     // w momentum rho*w*Vj+ pAz
-    b.jF(i,j,k,3) =                               rho * 0.5*(b.q(i,j,k,3)+b.q(i,j-1,k,3)) * V
-                  + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) *      b.jsz(i,j,k)                   ;
+    b.jF(i,j,k,3) = rho * 0.5*(b.q(i,j,k,3)+b.q(i,j-1,k,3)) * V
+                        + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) * b.jsz(i,j,k) ;
 
     // Total energy (rhoE+P)*Vj)
     double e;
@@ -181,16 +181,16 @@ for(block_ b : mb){
     b.kF(i,j,k,0) = rho * W;
 
     // x momentum rho*u*Wk+ pAx
-    b.kF(i,j,k,1) =                               rho * 0.5*(b.q(i,j,k,1)+b.q(i,j,k-1,1)) * W
-                  + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) *      b.ksx(i,j,k)                   ;
+    b.kF(i,j,k,1) = rho * 0.5*(b.q(i,j,k,1)+b.q(i,j,k-1,1)) * W
+                        + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) * b.ksx(i,j,k) ;
 
     // y momentum rho*v*Wk+ pAy
-    b.kF(i,j,k,2) =                               rho * 0.5*(b.q(i,j,k,2)+b.q(i,j,k-1,2)) * W
-                  + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) *      b.ksy(i,j,k)                   ;
+    b.kF(i,j,k,2) = rho * 0.5*(b.q(i,j,k,2)+b.q(i,j,k-1,2)) * W
+                        + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) * b.ksy(i,j,k) ;
 
     // w momentum rho*w*Wk+ pAz
-    b.kF(i,j,k,3) =                               rho * 0.5*(b.q(i,j,k,3)+b.q(i,j,k-1,3)) * W
-                  + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) *      b.ksz(i,j,k)                   ;
+    b.kF(i,j,k,3) = rho * 0.5*(b.q(i,j,k,3)+b.q(i,j,k-1,3)) * W
+                        + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) * b.ksz(i,j,k) ;
 
     // Total energy (rhoE+P)*Wk)
     double e;
