da1f817e3afc7e67672772cc423fc57048c59119
Kyle Schau <kschau@gatech.edu>
2021-09-05 21:08:52 -0400

    Updated examples to new species stuff. Removed accidental file.

--- examples/oneDadvec.py
+++ examples/oneDadvec.py
@@ -36,7 +36,6 @@ def simulate():
     config = pg.files.config_file()
     config['RHS']['diffusion'] = False
     mb = pg.multiblock.generate_multiblock_solver(1,config)
-    therm = pg.thermo.thermdat(config)
     pg.grid.create.multiblock_cube(mb,
                                    mb_dimensions=[1,1,1],
                                    dimensions_perblock=[41,2,2],
