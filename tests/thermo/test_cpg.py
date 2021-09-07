be9516a00d5130431236f2ff5c55996c9752f7a9
Kyle Schau <kschau@gatech.edu>
2021-09-06 21:17:01 -0400

    Can now use absolute path for specifying sp_data. Need to do this
    in all tests be of how pytests works.

--- tests/thermo/test_cpg.py
+++ tests/thermo/test_cpg.py
@@ -17,6 +17,7 @@ def test_cpg():
 
     relpath = str(Path(__file__).parent)
     ctfile = relpath+'/ct_test_cpg.yaml'
+    thfile = relpath+'/thtr_ct_test_cpg.yaml'
     gas = ct.Solution(ctfile)
     p = np.random.uniform(low=10000, high=100000)
     T = np.random.uniform(low=100  , high=1000)
@@ -26,7 +27,7 @@ def test_cpg():
     gas.TPY = T,p,Y
 
     config = pg.files.config_file()
-    config['thermochem']['spdata'] = './thtr_ct_test_cpg.yaml'
+    config['thermochem']['spdata'] = thfile
     config['thermochem']['eos'] = 'cpg'
     config['RHS']['diffusion'] = False
 
