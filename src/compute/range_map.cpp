17a912d2e7680ae210ca6e67b007d83487c232ae
Kyle A. Schau <kaschau89@gmail.com>
2021-09-22 18:33:09 -0400

    Need a break statement in switch.

--- src/compute/range_map.cpp
+++ src/compute/range_map.cpp
@@ -1,6 +1,6 @@
 #include "kokkos_types.hpp"
 #include "block_.hpp"
-#include <string>
+#include <stdexcept>
 
 MDRange3 get_range3(block_ b,
                        int face,
@@ -15,30 +15,41 @@ MDRange3 get_range3(block_ b,
       case -1 :
         // total block
         range = MDRange3({0,0,0},{b.ni+1,b.nj+1,b.nk+1});
+        break;
       case 0 :
         // interior
         range = MDRange3({1,1,1},{b.ni,b.nj,b.nk});
+        break;
       case 1 :
         // face 1 halo
         range = MDRange3({0,0,0},{1,b.nj+1,b.nk+1});
+        break;
       case 2 :
         // face 2 halo
         range = MDRange3({b.ni,0,0},{b.ni+1,b.nj+1,b.nk+1});
+        break;
       case 3 :
         // face 3 halo
         range = MDRange3({0,0,0},{b.ni+1,1,b.nk+1});
+        break;
       case 4 :
         // face 4 halo
         range = MDRange3({0,b.nj,0},{b.ni+1,b.nj+1,b.nk+1});
+        break;
       case 5 :
         // face 5 halo
         range = MDRange3({0,0,0},{b.ni+1,b.nj+1,1});
+        break;
       case 6 :
         // face 6 halo
         range = MDRange3({0,0,b.nk},{b.ni+1,b.nj+1,b.nk+1});
+        break;
       case 10 :
         // specify i,j,k turn it into a function call (kinda)
-          range = MDRange3({i,j,k},{i,j,k});
+        range = MDRange3({i,j,k},{i,j,k});
+        break;
+      default :
+        throw std::invalid_argument( "Unknown argument to get_range3");
     }
 
     return range;
