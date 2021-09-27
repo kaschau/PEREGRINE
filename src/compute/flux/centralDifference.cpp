65637fe43c73bce9aca9cbaf5bf87493d0bdcaf0
Kyle Schau <kschau@gatech.edu>
2021-09-27 08:40:13 -0400

    Reorg

--- src/compute/flux/centralDifference.cpp
+++ src/compute/flux/centralDifference.cpp
@@ -3,76 +3,110 @@
 #include "block_.hpp"
 #include "thtrdat_.hpp"
 
-void advective(block_ b, thtrdat_ th) {
+void diffusive(block_ b, thtrdat_ th) {
 
 //-------------------------------------------------------------------------------------------|
 // i flux face range
 //-------------------------------------------------------------------------------------------|
   MDRange3 range_i({1,1,1},{b.ni+1,b.nj,b.nk});
-  Kokkos::parallel_for("i face conv fluxes", range_i, KOKKOS_LAMBDA(const int i,
+  Kokkos::parallel_for("i face visc fluxes", range_i, KOKKOS_LAMBDA(const int i,
                                                                     const int j,
                                                                     const int k) {
-    double U;
-    double uf;
-    double vf;
-    double wf;
 
+    double dudx,dudy,dudz;
+    double dvdx,dvdy,dvdz;
+    double dwdx,dwdy,dwdz;
+
+    double txx,txy,txz;
+    double tyx,tyy,tyz;
+    double tzx,tzy,tzz;
+
+    double dTdx,dTdy,dTdz;
+    double uf,vf,wf;
+    double q;
+
+    double dNdx,dNdy,dNdz;
+
+    const double c23 = 2.0/3.0;
+
+    double mu     = 0.5*(b.qt(i,j,k,0) + b.qt(i-1,j,k,0));
+    double kappa  = 0.5*(b.qt(i,j,k,1) + b.qt(i-1,j,k,1));
+
+    // continuity
+    b.iF(i,j,k,0) = 0.0;
+
+    // Derivatives on face
+    dudx = 0.5*(b.dqdx(i,j,k,1) + b.dqdx(i-1,j,k,1) );
+    dvdx = 0.5*(b.dqdx(i,j,k,2) + b.dqdx(i-1,j,k,2) );
+    dwdx = 0.5*(b.dqdx(i,j,k,3) + b.dqdx(i-1,j,k,3) );
+
+    dudy = 0.5*(b.dqdy(i,j,k,1) + b.dqdy(i-1,j,k,1) );
+    dvdy = 0.5*(b.dqdy(i,j,k,2) + b.dqdy(i-1,j,k,2) );
+    dwdy = 0.5*(b.dqdy(i,j,k,3) + b.dqdy(i-1,j,k,3) );
+
+    dudz = 0.5*(b.dqdz(i,j,k,1) + b.dqdz(i-1,j,k,1) );
+    dvdz = 0.5*(b.dqdz(i,j,k,2) + b.dqdz(i-1,j,k,2) );
+    dwdz = 0.5*(b.dqdz(i,j,k,3) + b.dqdz(i-1,j,k,3) );
+
+    // x momentum
+    txx = c23*mu*(2.0*dudx - dvdy - dwdz);
+    txy =     mu*(    dvdx + dudy       );
+    txz =     mu*(    dwdx        + dudy);
+
+    b.iF(i,j,k,1) = - ( txx * b.isx(i,j,k) +
+                        txy * b.isy(i,j,k) +
+                        txz * b.isz(i,j,k) );
+
+    // y momentum
+    tyx = txy;
+    tyy = c23*mu*(-dudx +2.0*dvdy - dwdz);
+    tyz =     mu*(           dwdy + dvdz);
+
+    b.iF(i,j,k,2) = - ( tyx * b.isx(i,j,k) +
+                        tyy * b.isy(i,j,k) +
+                        tyz * b.isz(i,j,k) );
+
+    // z momentum
+    tzx = txz;
+    tzy = tyz;
+    tzz = c23*mu*(-dudx - dvdy +2.0*dwdz);
+
+    b.iF(i,j,k,3) = - ( tzx * b.isx(i,j,k) +
+                        tzy * b.isy(i,j,k) +
+                        tzz * b.isz(i,j,k) );
+
+    // energy
+    //   heat conduction
+    dTdx = 0.5 * ( b.dqdx(i,j,k,4) + b.dqdx(i-1,j,k,4) );
+    dTdy = 0.5 * ( b.dqdy(i,j,k,4) + b.dqdy(i-1,j,k,4) );
+    dTdz = 0.5 * ( b.dqdz(i,j,k,4) + b.dqdz(i-1,j,k,4) );
+
+    q =  kappa*( dTdx * b.isx(i,j,k) +
+                 dTdy * b.isy(i,j,k) +
+                 dTdz * b.isz(i,j,k) );
+
+    // flow work
     // Compute face normal volume flux vector
     uf = 0.5*(b.q(i,j,k,1)+b.q(i-1,j,k,1));
     vf = 0.5*(b.q(i,j,k,2)+b.q(i-1,j,k,2));
     wf = 0.5*(b.q(i,j,k,3)+b.q(i-1,j,k,3));
 
-    U = b.isx(i,j,k)*uf +
-        b.isy(i,j,k)*vf +
-        b.isz(i,j,k)*wf ;
+    b.iF(i,j,k,4) = ( uf*txx*b.isx(i,j,k) +
+                      vf*txy*b.isy(i,j,k) +
+                      wf*txz*b.isz(i,j,k) ) - q;
 
-    //Compute fluxes
-    double rho;
-    rho = 0.5*(b.Q(i,j,k,0)+b.Q(i-1,j,k,0));
-
-    // Continuity rho*Ui
-    b.iF(i,j,k,0) = rho * U;
-
-    // x momentum rho*u*Ui+ p*Ax
-    b.iF(i,j,k,1) = rho * 0.5*(b.q(i,j,k,1)+b.q(i-1,j,k,1)) * U
-                        + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) * b.isx(i,j,k) ;
-
-    // y momentum rho*v*Ui+ p*Ay
-    b.iF(i,j,k,2) = rho * 0.5*(b.q(i,j,k,2)+b.q(i-1,j,k,2)) * U
-                        + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) * b.isy(i,j,k) ;
-
-    // w momentum rho*w*Ui+ p*Az
-    b.iF(i,j,k,3) = rho * 0.5*(b.q(i,j,k,3)+b.q(i-1,j,k,3)) * U
-                        + 0.5*(b.q(i,j,k,0)+b.q(i-1,j,k,0)) * b.isz(i,j,k) ;
-
-    // Total energy (rhoE+ p)*Ui)
-    double e;
-    double em;
-
-    e = b.qh(i  ,j,k,4)/b.Q(i  ,j,k,0);
-
-
-
-    em= b.qh(i-1,j,k,4)/b.Q(i-1,j,k,0);
-
-
-
-    b.iF(i,j,k,4) =( rho *(0.5*(  e         +  em         )
-                         + 0.5*(b.q(i,j,k,1)*b.q(i-1,j,k,1)  +
-                                b.q(i,j,k,2)*b.q(i-1,j,k,2)  +
-                                b.q(i,j,k,3)*b.q(i-1,j,k,3)) ) ) * U;
-
-    b.iF(i,j,k,4)+= 0.5*(b.q(i-1,j,k,0)*(b.q(i  ,j,k,1)*b.isx(i,j,k)
-                                        +b.q(i  ,j,k,2)*b.isy(i,j,k)
-                                        +b.q(i  ,j,k,3)*b.isz(i,j,k) ) +
-                         b.q(i  ,j,k,0)*(b.q(i-1,j,k,1)*b.isx(i,j,k)
-                                        +b.q(i-1,j,k,2)*b.isy(i,j,k)
-                                        +b.q(i-1,j,k,3)*b.isz(i,j,k) ) );
     // Species
+    double Dij;
     for (int n=0; n<th.ns-1; n++)
     {
-      b.iF(i,j,k,5+n) = rho * 0.5*(b.q(i,j,k,5+n)+b.q(i-1,j,k,5+n)) * U;
-      //b.iF(i,j,k,5+n) = 0.5*(b.Q(i,j,k,5+n)+b.Q(i-1,j,k,5+n)) * U;
+      Dij  = 0.5 * ( b.qt(i,j,k,2+n) + b.qt(i-1,j,k,2+n)     );
+      dNdx = 0.5 * ( b.dqdx(i,j,k,5+n) + b.dqdx(i-1,j,k,5+n) );
+      dNdy = 0.5 * ( b.dqdy(i,j,k,5+n) + b.dqdy(i-1,j,k,5+n) );
+      dNdz = 0.5 * ( b.dqdz(i,j,k,5+n) + b.dqdz(i-1,j,k,5+n) );
+
+      b.iF(i,j,k,5+n) = -Dij*( dNdx * b.isx(i,j,k) +
+                               dNdy * b.isy(i,j,k) +
+                               dNdz * b.isz(i,j,k) );
     }
 
   });
@@ -81,71 +115,104 @@ void advective(block_ b, thtrdat_ th) {
 // j flux face range
 //-------------------------------------------------------------------------------------------|
   MDRange3 range_j({1,1,1},{b.ni,b.nj+1,b.nk});
-  Kokkos::parallel_for("j face conv fluxes", range_j, KOKKOS_LAMBDA(const int i,
+  Kokkos::parallel_for("j face visc fluxes", range_j, KOKKOS_LAMBDA(const int i,
                                                                     const int j,
                                                                     const int k) {
 
-    double V;
-    double uf;
-    double vf;
-    double wf;
+    double dudx,dudy,dudz;
+    double dvdx,dvdy,dvdz;
+    double dwdx,dwdy,dwdz;
 
+    double txx,txy,txz;
+    double tyx,tyy,tyz;
+    double tzx,tzy,tzz;
+
+    double dTdx,dTdy,dTdz;
+    double uf,vf,wf;
+    double q;
+
+    double dNdx,dNdy,dNdz;
+
+    const double c23 = 2.0/3.0;
+
+    double mu     = 0.5*(b.qt(i,j,k,0) + b.qt(i,j-1,k,0));
+    double kappa  = 0.5*(b.qt(i,j,k,1) + b.qt(i,j-1,k,1));
+
+    // continuity
+    b.jF(i,j,k,0) = 0.0;
+
+    // Spatial derivative on face
+    dudx = 0.5*(b.dqdx(i,j,k,1) + b.dqdx(i,j-1,k,1) );
+    dvdx = 0.5*(b.dqdx(i,j,k,2) + b.dqdx(i,j-1,k,2) );
+    dwdx = 0.5*(b.dqdx(i,j,k,3) + b.dqdx(i,j-1,k,3) );
+
+    dudy = 0.5*(b.dqdy(i,j,k,1) + b.dqdy(i,j-1,k,1) );
+    dvdy = 0.5*(b.dqdy(i,j,k,2) + b.dqdy(i,j-1,k,2) );
+    dwdy = 0.5*(b.dqdy(i,j,k,3) + b.dqdy(i,j-1,k,3) );
+
+    dudz = 0.5*(b.dqdz(i,j,k,1) + b.dqdz(i,j-1,k,1) );
+    dvdz = 0.5*(b.dqdz(i,j,k,2) + b.dqdz(i,j-1,k,2) );
+    dwdz = 0.5*(b.dqdz(i,j,k,3) + b.dqdz(i,j-1,k,3) );
+
+    // x momentum
+    txx = c23*mu*(2.0*dudx - dvdy - dwdz);
+    txy =     mu*(    dvdx + dudy       );
+    txz =     mu*(    dwdx        + dudy);
+
+    b.jF(i,j,k,1) = - ( txx * b.jsx(i,j,k) +
+                        txy * b.jsy(i,j,k) +
+                        txz * b.jsz(i,j,k) );
+
+    // y momentum
+    tyx = txy;
+    tyy = c23*mu*(-dudx +2.0*dvdy - dwdz);
+    tyz =     mu*(           dwdy + dvdz);
+
+    b.jF(i,j,k,2) = - ( tyx * b.jsx(i,j,k) +
+                        tyy * b.jsy(i,j,k) +
+                        tyz * b.jsz(i,j,k) );
+
+    // z momentum
+    tzx = txz;
+    tzy = tyz;
+    tzz = c23*mu*(-dudx - dvdy +2.0*dwdz);
+
+    b.jF(i,j,k,3) = - ( tzx * b.jsx(i,j,k) +
+                        tzy * b.jsy(i,j,k) +
+                        tzz * b.jsz(i,j,k) );
+
+    // energy
+    //   heat conduction
+    dTdx = 0.5 * ( b.dqdx(i,j,k,4) + b.dqdx(i,j-1,k,4) );
+    dTdy = 0.5 * ( b.dqdy(i,j,k,4) + b.dqdy(i,j-1,k,4) );
+    dTdz = 0.5 * ( b.dqdz(i,j,k,4) + b.dqdz(i,j-1,k,4) );
+
+    q =  kappa*( dTdx * b.jsx(i,j,k) +
+                 dTdy * b.jsy(i,j,k) +
+                 dTdz * b.jsz(i,j,k) );
+
+    // flow work
     // Compute face normal volume flux vector
     uf = 0.5*(b.q(i,j,k,1)+b.q(i,j-1,k,1));
     vf = 0.5*(b.q(i,j,k,2)+b.q(i,j-1,k,2));
     wf = 0.5*(b.q(i,j,k,3)+b.q(i,j-1,k,3));
 
-    V = b.jsx(i,j,k)*uf +
-        b.jsy(i,j,k)*vf +
-        b.jsz(i,j,k)*wf ;
+    b.jF(i,j,k,4) = ( uf*txx*b.jsx(i,j,k) +
+                      vf*txy*b.jsy(i,j,k) +
+                      wf*txz*b.jsz(i,j,k) ) - q;
 
-    //Compute fluxes
-    double rho;
-    rho = 0.5*(b.Q(i,j,k,0)+b.Q(i,j-1,k,0));
-
-    // Continuity rho*Vj
-    b.jF(i,j,k,0) = rho * V;
-
-    // x momentum rho*u*Vj+ pAx
-    b.jF(i,j,k,1) = rho * 0.5*(b.q(i,j,k,1)+b.q(i,j-1,k,1)) * V
-                        + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) * b.jsx(i,j,k) ;
-
-    // y momentum rho*v*Vj+ pAy
-    b.jF(i,j,k,2) = rho * 0.5*(b.q(i,j,k,2)+b.q(i,j-1,k,2)) * V
-                        + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) * b.jsy(i,j,k) ;
-
-    // w momentum rho*w*Vj+ pAz
-    b.jF(i,j,k,3) = rho * 0.5*(b.q(i,j,k,3)+b.q(i,j-1,k,3)) * V
-                        + 0.5*(b.q(i,j,k,0)+b.q(i,j-1,k,0)) * b.jsz(i,j,k) ;
-
-    // Total energy (rhoE+P)*Vj)
-    double e;
-    double em;
-
-    e = b.qh(i,j  ,k,4)/b.Q(i,j  ,k,0);
-
-
-
-    em= b.qh(i,j-1,k,4)/b.Q(i,j-1,k,0);
-
-
-
-    b.jF(i,j,k,4) =( rho *(0.5*(  e         +  em         )
-                         + 0.5*(b.q(i,j,k,1)*b.q(i,j-1,k,1)  +
-                                b.q(i,j,k,2)*b.q(i,j-1,k,2)  +
-                                b.q(i,j,k,3)*b.q(i,j-1,k,3)) ) ) * V;
-
-    b.jF(i,j,k,4)+= 0.5*(b.q(i,j-1,k,0)*(b.q(i,j  ,k,1)*b.jsx(i,j,k)
-                                        +b.q(i,j  ,k,2)*b.jsy(i,j,k)
-                                        +b.q(i,j  ,k,3)*b.jsz(i,j,k) ) +
-                         b.q(i,j  ,k,0)*(b.q(i,j-1,k,1)*b.jsx(i,j,k)
-                                        +b.q(i,j-1,k,2)*b.jsy(i,j,k)
-                                        +b.q(i,j-1,k,3)*b.jsz(i,j,k) ) );
     // Species
+    double Dij;
     for (int n=0; n<th.ns-1; n++)
     {
-      b.jF(i,j,k,5+n) = rho * 0.5*(b.q(i,j,k,5+n)+b.q(i,j-1,k,5+n)) * V;
-      //b.jF(i,j,k,5+n) = 0.5*(b.Q(i,j,k,5+n)+b.Q(i,j-1,k,5+n)) * V;
+      Dij  = 0.5 * ( b.qt(i,j,k,2+n) + b.qt(i,j-1,k,2+n)     );
+      dNdx = 0.5 * ( b.dqdx(i,j,k,5+n) + b.dqdx(i,j-1,k,5+n) );
+      dNdy = 0.5 * ( b.dqdy(i,j,k,5+n) + b.dqdy(i,j-1,k,5+n) );
+      dNdz = 0.5 * ( b.dqdz(i,j,k,5+n) + b.dqdz(i,j-1,k,5+n) );
+
+      b.jF(i,j,k,5+n) = -Dij*( dNdx * b.jsx(i,j,k) +
+                               dNdy * b.jsy(i,j,k) +
+                               dNdz * b.jsz(i,j,k) );
     }
 
   });
@@ -154,70 +221,104 @@ void advective(block_ b, thtrdat_ th) {
 // k flux face range
 //-------------------------------------------------------------------------------------------|
   MDRange3 range_k({1,1,1},{b.ni,b.nj,b.nk+1});
-  Kokkos::parallel_for("k face conv fluxes", range_k, KOKKOS_LAMBDA(const int i,
+  Kokkos::parallel_for("k face visc fluxes", range_k, KOKKOS_LAMBDA(const int i,
                                                                     const int j,
                                                                     const int k) {
 
-    double W;
-    double uf;
-    double vf;
-    double wf;
+    double dudx,dudy,dudz;
+    double dvdx,dvdy,dvdz;
+    double dwdx,dwdy,dwdz;
 
+    double txx,txy,txz;
+    double tyx,tyy,tyz;
+    double tzx,tzy,tzz;
+
+    double dTdx,dTdy,dTdz;
+    double uf,vf,wf;
+    double q;
+
+    double dNdx,dNdy,dNdz;
+
+    const double c23 = 2.0/3.0;
+
+    double mu     = 0.5*(b.qt(i,j,k,0) + b.qt(i,j,k-1,0));
+    double kappa  = 0.5*(b.qt(i,j,k,1) + b.qt(i,j,k-1,1));
+
+    // continuity
+    b.kF(i,j,k,0) = 0.0;
+
+    // Spatial derivative on face
+    dudx = 0.5*(b.dqdx(i,j,k,1) + b.dqdx(i,j,k-1,1) );
+    dvdx = 0.5*(b.dqdx(i,j,k,2) + b.dqdx(i,j,k-1,2) );
+    dwdx = 0.5*(b.dqdx(i,j,k,3) + b.dqdx(i,j,k-1,3) );
+
+    dudy = 0.5*(b.dqdy(i,j,k,1) + b.dqdy(i,j,k-1,1) );
+    dvdy = 0.5*(b.dqdy(i,j,k,2) + b.dqdy(i,j,k-1,2) );
+    dwdy = 0.5*(b.dqdy(i,j,k,3) + b.dqdy(i,j,k-1,3) );
+
+    dudz = 0.5*(b.dqdz(i,j,k,1) + b.dqdz(i,j,k-1,1) );
+    dvdz = 0.5*(b.dqdz(i,j,k,2) + b.dqdz(i,j,k-1,2) );
+    dwdz = 0.5*(b.dqdz(i,j,k,3) + b.dqdz(i,j,k-1,3) );
+
+    // x momentum
+    txx = c23*mu*(2.0*dudx - dvdy - dwdz);
+    txy =     mu*(    dvdx + dudy       );
+    txz =     mu*(    dwdx        + dudy);
+
+    b.kF(i,j,k,1) = - ( txx * b.ksx(i,j,k) +
+                        txy * b.ksy(i,j,k) +
+                        txz * b.ksz(i,j,k) );
+
+    // y momentum
+    tyx = txy;
+    tyy = c23*mu*(-dudx +2.0*dvdy - dwdz);
+    tyz =     mu*(           dwdy + dvdz);
+
+    b.kF(i,j,k,2) = - ( tyx * b.ksx(i,j,k) +
+                        tyy * b.ksy(i,j,k) +
+                        tyz * b.ksz(i,j,k) );
+
+    // z momentum
+    tzx = txz;
+    tzy = tyz;
+    tzz = c23*mu*(-dudx - dvdy +2.0*dwdz);
+
+    b.kF(i,j,k,3) = - ( tzx * b.ksx(i,j,k) +
+                        tzy * b.ksy(i,j,k) +
+                        tzz * b.ksz(i,j,k) );
+
+    // energy
+    //   heat conduction
+    dTdx = 0.5 * ( b.dqdx(i,j,k,4) + b.dqdx(i,j,k-1,4) );
+    dTdy = 0.5 * ( b.dqdy(i,j,k,4) + b.dqdy(i,j,k-1,4) );
+    dTdz = 0.5 * ( b.dqdz(i,j,k,4) + b.dqdz(i,j,k-1,4) );
+
+    q =  kappa*( dTdx * b.ksx(i,j,k) +
+                 dTdy * b.ksy(i,j,k) +
+                 dTdz * b.ksz(i,j,k) );
+
+    // flow work
     // Compute face normal volume flux vector
     uf = 0.5*(b.q(i,j,k,1)+b.q(i,j,k-1,1));
     vf = 0.5*(b.q(i,j,k,2)+b.q(i,j,k-1,2));
     wf = 0.5*(b.q(i,j,k,3)+b.q(i,j,k-1,3));
 
-    W = b.ksx(i,j,k)*uf +
-        b.ksy(i,j,k)*vf +
-        b.ksz(i,j,k)*wf ;
+    b.kF(i,j,k,4) = ( uf*txx*b.ksx(i,j,k) +
+                      vf*txy*b.ksy(i,j,k) +
+                      wf*txz*b.ksz(i,j,k) ) - q;
 
-    //Compute fluxes
-    double rho;
-    rho = 0.5*(b.Q(i,j,k,0)+b.Q(i,j,k-1,0));
-    // Continuity rho*Wk
-    b.kF(i,j,k,0) = rho * W;
-
-    // x momentum rho*u*Wk+ pAx
-    b.kF(i,j,k,1) = rho * 0.5*(b.q(i,j,k,1)+b.q(i,j,k-1,1)) * W
-                        + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) * b.ksx(i,j,k) ;
-
-    // y momentum rho*v*Wk+ pAy
-    b.kF(i,j,k,2) = rho * 0.5*(b.q(i,j,k,2)+b.q(i,j,k-1,2)) * W
-                        + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) * b.ksy(i,j,k) ;
-
-    // w momentum rho*w*Wk+ pAz
-    b.kF(i,j,k,3) = rho * 0.5*(b.q(i,j,k,3)+b.q(i,j,k-1,3)) * W
-                        + 0.5*(b.q(i,j,k,0)+b.q(i,j,k-1,0)) * b.ksz(i,j,k) ;
-
-    // Total energy (rhoE+P)*Wk)
-    double e;
-    double em;
-
-    e = b.qh(i,j,k  ,4)/b.Q(i,j,k  ,0);
-
-
-
-    em= b.qh(i,j,k-1,4)/b.Q(i,j,k-1,0);
-
-
-
-    b.kF(i,j,k,4) =( rho *(0.5*(  e         +  em         )
-                         + 0.5*(b.q(i,j,k,1)*b.q(i,j,k-1,1)  +
-                                b.q(i,j,k,2)*b.q(i,j,k-1,2)  +
-                                b.q(i,j,k,3)*b.q(i,j,k-1,3)) ) ) * W;
-
-    b.kF(i,j,k,4)+= 0.5*(b.q(i,j,k-1,0)*(b.q(i,j,k  ,1)*b.ksx(i,j,k)
-                                        +b.q(i,j,k  ,2)*b.ksy(i,j,k)
-                                        +b.q(i,j,k  ,3)*b.ksz(i,j,k) ) +
-                         b.q(i,j,k  ,0)*(b.q(i,j,k-1,1)*b.ksx(i,j,k)
-                                        +b.q(i,j,k-1,2)*b.ksy(i,j,k)
-                                        +b.q(i,j,k-1,3)*b.ksz(i,j,k) ) );
     // Species
+    double Dij;
     for (int n=0; n<th.ns-1; n++)
     {
-      b.kF(i,j,k,5+n) = rho * 0.5*(b.q(i,j,k,5+n)+b.q(i,j,k-1,5+n)) * W;
-      //b.kF(i,j,k,5+n) = 0.5*(b.Q(i,j,k,5+n)+b.Q(i,j,k-1,5+n)) * W;
+      Dij  = 0.5 * ( b.qt(i,j,k,2+n) + b.qt(i,j,k-1,2+n)     );
+      dNdx = 0.5 * ( b.dqdx(i,j,k,5+n) + b.dqdx(i,j,k-1,5+n) );
+      dNdy = 0.5 * ( b.dqdy(i,j,k,5+n) + b.dqdy(i,j,k-1,5+n) );
+      dNdz = 0.5 * ( b.dqdz(i,j,k,5+n) + b.dqdz(i,j,k-1,5+n) );
+
+      b.kF(i,j,k,5+n) = -Dij*( dNdx * b.ksx(i,j,k) +
+                               dNdy * b.ksy(i,j,k) +
+                               dNdz * b.ksz(i,j,k) );
     }
 
   });
