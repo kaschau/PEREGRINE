4e33b50b93948c05e40baa1d2f567c63bf385f7a
Kyle Schau <kschau@gatech.edu>
2021-09-30 14:32:54 -0400

    Added j and k rusanov flux.

--- src/compute/advFlux/rusanov.cpp
+++ src/compute/advFlux/rusanov.cpp
@@ -85,7 +85,7 @@ void rusanov(block_ b, const thtrdat_ th, const double primary) {
       FYiL = rhoL*b.q(i-1,j,k,5+n)*UL;
       YiR = b.Q(i  ,j,k,5+n);
       YiL = b.Q(i-1,j,k,5+n);
-      b.iF(i,j,k,5+n) = 0.5*( FYiR + FYiL - lam*(YiR - YiL) ) * b.iS(i,j,k);
+      b.iF(i,j,k,5+n) = 0.5*( FYiR + FYiL - lam*(YiR - YiL)*b.iS(i,j,k) );
     }
 
   });
@@ -98,6 +98,82 @@ void rusanov(block_ b, const thtrdat_ th, const double primary) {
                                                                     const int j,
                                                                     const int k) {
 
+    double ufR, vfR, wfR;
+    double VR;
+    double rhoR, pR, ER;
+
+    double ufL, vfL, wfL;
+    double VL;
+    double rhoL, pL, EL;
+
+    ufR = b.q(i,j  ,k,1);
+    vfR = b.q(i,j  ,k,2);
+    wfR = b.q(i,j  ,k,3);
+
+    ufL = b.q(i,j-1,k,1);
+    vfL = b.q(i,j-1,k,2);
+    wfL = b.q(i,j-1,k,3);
+
+    VR = b.jsx(i,j,k)*ufR +
+         b.jsy(i,j,k)*vfR +
+         b.jsz(i,j,k)*wfR ;
+    VL = b.jsx(i,j,k)*ufL +
+         b.jsy(i,j,k)*vfL +
+         b.jsz(i,j,k)*wfL ;
+
+    rhoR = b.Q(i,j  ,k,0);
+    rhoL = b.Q(i,j-1,k,0);
+
+    pR = b.q(i,j  ,k,0);
+    pL = b.q(i,j-1,k,0);
+
+    ER = b.Q(i,j  ,k,4);
+    EL = b.Q(i,j-1,k,4);
+
+    // wave speed estimate / 2
+    //double lam = 0.25*( ( b.qh(i,j,k,3) + b.qh(i-1,j,k,3) ) + abs(UR + UL) );
+    double lam = fmax( abs(VL) + b.qh(i,j,k,3), abs(VR) + b.qh(i,j-1,k,3) );
+
+    // Continuity rho*Ui
+    double FrhoR,FrhoL;
+    FrhoR = VR*rhoR;
+    FrhoL = VL*rhoL;
+    b.jF(i,j,k,0) = 0.5*( FrhoR + FrhoL - lam*(rhoR - rhoL)*b.jS(i,j,k) );
+
+    double FUR,FUL;
+    // x momentum rho*u*Ui+ p*Ax
+    FUR = VR*ufR*rhoR + pR*b.jsx(i,j,k);
+    FUL = VL*ufL*rhoL + pL*b.jsx(i,j,k);
+    b.jF(i,j,k,1) = 0.5*( FUR + FUL - lam*(rhoR*ufR - rhoL*ufL)*b.jS(i,j,k) );
+
+    // y momentum rho*v*Ui+ p*Ay
+    FUR = VR*vfR*rhoR + pR*b.jsy(i,j,k);
+    FUL = VL*vfL*rhoL + pL*b.jsy(i,j,k);
+    b.jF(i,j,k,2) = 0.5*( FUR + FUL - lam*(rhoR*vfR - rhoL*vfL)*b.jS(i,j,k) );
+
+    // w momentum rho*w*Ui+ p*Az
+    FUR = VR*wfR*rhoR + pR*b.jsz(i,j,k);
+    FUL = VL*wfL*rhoL + pL*b.jsz(i,j,k);
+    b.jF(i,j,k,3) = 0.5*( FUR + FUL - lam*(rhoR*wfR - rhoL*wfL)*b.jS(i,j,k) );
+
+    // Total energy (rhoE+ p)*Ui)
+    double FER, FEL;
+    FER = VR*(ER + pR);
+    FEL = VL*(EL + pL);
+    b.jF(i,j,k,4) = 0.5*( FER + FEL - lam*(ER - EL)*b.jS(i,j,k) );
+
+    // Species
+    double FYiR, FYiL;
+    double YiR, YiL;
+    for (int n=0; n<th.ns-1; n++)
+    {
+      FYiR = rhoR*b.q(i,j  ,k,5+n)*VR;
+      FYiL = rhoL*b.q(i,j-1,k,5+n)*VL;
+      YiR = b.Q(i,j  ,k,5+n);
+      YiL = b.Q(i,j-1,k,5+n);
+      b.jF(i,j,k,5+n) = 0.5*( FYiR + FYiL - lam*(YiR - YiL)*b.jS(i,j,k) );
+    }
+
 
   });
 //-------------------------------------------------------------------------------------------|
@@ -108,6 +184,81 @@ void rusanov(block_ b, const thtrdat_ th, const double primary) {
                                                                     const int j,
                                                                     const int k) {
 
+    double ufR, vfR, wfR;
+    double WR;
+    double rhoR, pR, ER;
+
+    double ufL, vfL, wfL;
+    double WL;
+    double rhoL, pL, EL;
+
+    ufR = b.q(i,j,k  ,1);
+    vfR = b.q(i,j,k  ,2);
+    wfR = b.q(i,j,k  ,3);
+
+    ufL = b.q(i,j,k-1,1);
+    vfL = b.q(i,j,k-1,2);
+    wfL = b.q(i,j,k-1,3);
+
+    WR = b.ksx(i,j,k)*ufR +
+         b.ksy(i,j,k)*vfR +
+         b.ksz(i,j,k)*wfR ;
+    WL = b.ksx(i,j,k)*ufL +
+         b.ksy(i,j,k)*vfL +
+         b.ksz(i,j,k)*wfL ;
+
+    rhoR = b.Q(i,j,k  ,0);
+    rhoL = b.Q(i,j,k-1,0);
+
+    pR = b.q(i,j,k  ,0);
+    pL = b.q(i,j,k-1,0);
+
+    ER = b.Q(i,j,k  ,4);
+    EL = b.Q(i,j,k-1,4);
+
+    // wave speed estimate / 2
+    //double lam = 0.25*( ( b.qh(i,j,k,3) + b.qh(i-1,j,k,3) ) + abs(UR + UL) );
+    double lam = fmax( abs(WL) + b.qh(i,j,k,3), abs(WR) + b.qh(i,j,k-1,3) );
+
+    // Continuity rho*Ui
+    double FrhoR,FrhoL;
+    FrhoR = WR*rhoR;
+    FrhoL = WL*rhoL;
+    b.kF(i,j,k,0) = 0.5*( FrhoR + FrhoL - lam*(rhoR - rhoL)*b.kS(i,j,k) );
+
+    double FUR,FUL;
+    // x momentum rho*u*Ui+ p*Ax
+    FUR = WR*ufR*rhoR + pR*b.ksx(i,j,k);
+    FUL = WL*ufL*rhoL + pL*b.ksx(i,j,k);
+    b.kF(i,j,k,1) = 0.5*( FUR + FUL - lam*(rhoR*ufR - rhoL*ufL)*b.kS(i,j,k) );
+
+    // y momentum rho*v*Ui+ p*Ay
+    FUR = WR*vfR*rhoR + pR*b.ksy(i,j,k);
+    FUL = WL*vfL*rhoL + pL*b.ksy(i,j,k);
+    b.kF(i,j,k,2) = 0.5*( FUR + FUL - lam*(rhoR*vfR - rhoL*vfL)*b.kS(i,j,k) );
+
+    // w momentum rho*w*Ui+ p*Az
+    FUR = WR*wfR*rhoR + pR*b.ksz(i,j,k);
+    FUL = WL*wfL*rhoL + pL*b.ksz(i,j,k);
+    b.kF(i,j,k,3) = 0.5*( FUR + FUL - lam*(rhoR*wfR - rhoL*wfL)*b.kS(i,j,k) );
+
+    // Total energy (rhoE+ p)*Ui)
+    double FER, FEL;
+    FER = WR*(ER + pR);
+    FEL = WL*(EL + pL);
+    b.kF(i,j,k,4) = 0.5*( FER + FEL - lam*(ER - EL)*b.kS(i,j,k) );
+
+    // Species
+    double FYiR, FYiL;
+    double YiR, YiL;
+    for (int n=0; n<th.ns-1; n++)
+    {
+      FYiR = rhoR*b.q(i,j,k  ,5+n)*WR;
+      FYiL = rhoL*b.q(i,j,k-1,5+n)*WL;
+      YiR = b.Q(i,j,k  ,5+n);
+      YiL = b.Q(i,j,k-1,5+n);
+      b.kF(i,j,k,5+n) = 0.5*( FYiR + FYiL - lam*(YiR - YiL)*b.kS(i,j,k) );
+    }
 
   });
 //-------------------------------------------------------------------------------------------|
