071761a9a2bc8fc72d81835821bed297215504a2
Kyle Schau <kschau@gatech.edu>
2021-10-11 22:35:52 -0400

    Made boundary conditions go through slice lists.

--- src/peregrinepy/bcs/exits.py
+++ src/peregrinepy/bcs/exits.py
@@ -3,19 +3,21 @@ def constantPressureSubsonicExit(eos, blk, face, thtrdat, terms):
     nface = face.nface
 
     if terms == "euler":
-        # Extrapolate everything
         q = blk.array["q"][:, :, :, 1::]
-        q[face.s0_] = 2.0 * q[face.s1_] - q[face.s2_]
-
-        # Set pressure at face
         p = blk.array["q"][:, :, :, 0]
-        p[face.s0_] = 2.0 * face.bcVals["p"] - p[face.s1_]
+        for s0_, s2_ in zip(face.s0_, face.s2_):
+            # Extrapolate everything
+            q[s0_] = 2.0 * q[face.s1_] - q[s2_]
+
+            # Set pressure at face
+            p[s0_] = 2.0 * face.bcVals["p"] - p[face.s1_]
 
         # Update conservatives
         eos(blk, thtrdat, nface, "prims")
 
     elif terms == "viscous":
-        # neumann all gradients
-        blk.array["dqdx"][face.s0_] = blk.array["dqdx"][face.s1_]
-        blk.array["dqdy"][face.s0_] = blk.array["dqdy"][face.s1_]
-        blk.array["dqdz"][face.s0_] = blk.array["dqdz"][face.s1_]
+        for s0_ in face.s0_:
+            # neumann all gradients
+            blk.array["dqdx"][s0_] = blk.array["dqdx"][face.s1_]
+            blk.array["dqdy"][s0_] = blk.array["dqdy"][face.s1_]
+            blk.array["dqdz"][s0_] = blk.array["dqdz"][face.s1_]
