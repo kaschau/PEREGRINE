bfac5d2482386a5e0b7ab96e5947c149c66b080a
Kyle A. Schau <kaschau89@gmail.com>
2021-10-11 22:35:52 -0400

    Made boundary conditions go through slice lists.

--- src/peregrinepy/bcs/inlets.py
+++ src/peregrinepy/bcs/inlets.py
@@ -3,30 +3,33 @@ def constantVelocitySubsonicInlet(eos, blk, face, thtrdat, terms):
     nface = face.nface
 
     if terms == "euler":
-        # extrapolate pressure
         p = blk.array["q"][:, :, :, 0]
-        p[face.s0_] = 2.0 * p[face.s1_] - p[face.s2_]
-
-        # apply velo on face
         u = blk.array["q"][:, :, :, 1]
         v = blk.array["q"][:, :, :, 2]
         w = blk.array["q"][:, :, :, 3]
-        u[face.s0_] = 2.0 * face.bcVals["u"] - u[face.s1_]
-        v[face.s0_] = 2.0 * face.bcVals["v"] - v[face.s1_]
-        w[face.s0_] = 2.0 * face.bcVals["w"] - w[face.s1_]
-
         T = blk.array["q"][:, :, :, 4]
-        T[face.s0_] = 2.0 * face.bcVals["T"] - T[face.s1_]
 
-        for n, sn in enumerate(thtrdat.speciesNames[0:-1]):
-            N = blk.array["q"][:, :, :, 5 + n]
-            N[face.s0_] = 2.0 * face.bcVals[sn] - N[face.s1_]
+        for s0_, s2_ in zip(face.s0_, face.s2_):
+            # extrapolate pressure
+            p[s0_] = 2.0 * p[face.s1_] - p[s2_]
+
+            # apply velo on face
+            u[s0_] = 2.0 * face.bcVals["u"] - u[face.s1_]
+            v[s0_] = 2.0 * face.bcVals["v"] - v[face.s1_]
+            w[s0_] = 2.0 * face.bcVals["w"] - w[face.s1_]
+
+            T[s0_] = 2.0 * face.bcVals["T"] - T[face.s1_]
+
+            for n, sn in enumerate(thtrdat.speciesNames[0:-1]):
+                N = blk.array["q"][:, :, :, 5 + n]
+                N[s0_] = 2.0 * face.bcVals[sn] - N[face.s1_]
 
         # Update conserved
         eos(blk, thtrdat, nface, "prims")
 
     elif terms == "viscous":
-        # neumann all gradients
-        blk.array["dqdx"][face.s0_] = blk.array["dqdx"][face.s1_]
-        blk.array["dqdy"][face.s0_] = blk.array["dqdy"][face.s1_]
-        blk.array["dqdz"][face.s0_] = blk.array["dqdz"][face.s1_]
+        for s0_ in face.s0_:
+            # neumann all gradients
+            blk.array["dqdx"][s0_] = blk.array["dqdx"][face.s1_]
+            blk.array["dqdy"][s0_] = blk.array["dqdy"][face.s1_]
+            blk.array["dqdz"][s0_] = blk.array["dqdz"][face.s1_]
--- src/peregrinepy/bcs/walls.py
+++ src/peregrinepy/bcs/walls.py
@@ -4,32 +4,34 @@ def adiabaticNoSlipWall(eos, blk, face, thtrdat, terms):
 
     if terms == "euler":
         p = blk.array["q"][:, :, :, 0]
-        p[face.s0_] = p[face.s1_]
-
         u = blk.array["q"][:, :, :, 1:4]
-        u[face.s0_] = -u[face.s1_]
-
         TN = blk.array["q"][:, :, :, 4::]
-        TN[face.s0_] = TN[face.s1_]
+        for s0_ in face.s0_:
+            p[s0_] = p[face.s1_]
+            u[s0_] = -u[face.s1_]
+            TN[s0_] = TN[face.s1_]
 
         # Update conservatives
         eos(blk, thtrdat, nface, "prims")
 
     elif terms == "viscous":
-        # extrapolate velocity gradient
         dvelodx = blk.array["dqdx"][:, :, :, 1:4]
         dvelody = blk.array["dqdy"][:, :, :, 1:4]
         dvelodz = blk.array["dqdz"][:, :, :, 1:4]
-        dvelodx[face.s0_] = 2.0 * dvelodx[face.s1_] - dvelodx[face.s2_]
-        dvelody[face.s0_] = 2.0 * dvelody[face.s1_] - dvelody[face.s2_]
-        dvelodz[face.s0_] = 2.0 * dvelodz[face.s1_] - dvelodz[face.s2_]
-        # negate temp and species gradient (so gradient evaluates to zero on wall)
+
         dTNdx = blk.array["dqdx"][:, :, :, 4::]
         dTNdy = blk.array["dqdy"][:, :, :, 4::]
         dTNdz = blk.array["dqdz"][:, :, :, 4::]
-        dTNdx[face.s0_] = -dTNdx[face.s1_]
-        dTNdy[face.s0_] = -dTNdy[face.s1_]
-        dTNdz[face.s0_] = -dTNdz[face.s1_]
+
+        for s0_, s2_ in zip(face.s0_, face.s2_):
+            # extrapolate velocity gradient
+            dvelodx[s0_] = 2.0 * dvelodx[face.s1_] - dvelodx[s2_]
+            dvelody[s0_] = 2.0 * dvelody[face.s1_] - dvelody[s2_]
+            dvelodz[s0_] = 2.0 * dvelodz[face.s1_] - dvelodz[s2_]
+            # negate temp and species gradient (so gradient evaluates to zero on wall)
+            dTNdx[s0_] = -dTNdx[face.s1_]
+            dTNdy[s0_] = -dTNdy[face.s1_]
+            dTNdz[s0_] = -dTNdz[face.s1_]
 
 
 def adiabaticSlipWall(eos, blk, face, thtrdat, terms):
@@ -38,8 +40,6 @@ def adiabaticSlipWall(eos, blk, face, thtrdat, terms):
 
     if terms == "euler":
         p = blk.array["q"][:, :, :, 0]
-        p[face.s0_] = p[face.s1_]
-
         u = blk.array["q"][:, :, :, 1]
         v = blk.array["q"][:, :, :, 2]
         w = blk.array["q"][:, :, :, 3]
@@ -57,51 +57,58 @@ def adiabaticSlipWall(eos, blk, face, thtrdat, terms):
             nz = blk.array["knz"]
         else:
             raise ValueError("Unknown nface")
-        u[face.s0_] = u[face.s1_] - 2.0 * u[face.s1_] * nx[face.s1_]
-        v[face.s0_] = v[face.s1_] - 2.0 * v[face.s1_] * ny[face.s1_]
-        w[face.s0_] = w[face.s1_] - 2.0 * w[face.s1_] * nz[face.s1_]
 
-        TN = blk.array["q"][:, :, :, 4::]
-        TN[face.s0_] = TN[face.s1_]
+        for s0_ in face.s0_:
+            p[s0_] = p[face.s1_]
+
+            u[s0_] = u[face.s1_] - 2.0 * u[face.s1_] * nx[face.s1_]
+            v[s0_] = v[face.s1_] - 2.0 * v[face.s1_] * ny[face.s1_]
+            w[s0_] = w[face.s1_] - 2.0 * w[face.s1_] * nz[face.s1_]
+
+            TN = blk.array["q"][:, :, :, 4::]
+            TN[s0_] = TN[face.s1_]
 
         # Update conservatives
         eos(blk, thtrdat, nface, "prims")
 
     elif terms == "viscous":
-        # negate velocity gradient (so gradient evaluates to zero on wall)
+
         dvelodx = blk.array["dqdx"][:, :, :, 1:4]
         dvelody = blk.array["dqdy"][:, :, :, 1:4]
         dvelodz = blk.array["dqdz"][:, :, :, 1:4]
-        dvelodx[face.s0_] = -dvelodx[face.s1_]
-        dvelody[face.s0_] = -dvelody[face.s1_]
-        dvelodz[face.s0_] = -dvelodz[face.s1_]
-        # negate temp and species gradient (so gradient evaluates to zero on wall)
         dTNdx = blk.array["dqdx"][:, :, :, 4::]
         dTNdy = blk.array["dqdy"][:, :, :, 4::]
         dTNdz = blk.array["dqdz"][:, :, :, 4::]
-        dTNdx[face.s0_] = -dTNdx[face.s1_]
-        dTNdy[face.s0_] = -dTNdy[face.s1_]
-        dTNdz[face.s0_] = -dTNdz[face.s1_]
+
+        for s0_ in face.s0_:
+            # negate velocity gradient (so gradient evaluates to zero on wall)
+            dvelodx[s0_] = -dvelodx[face.s1_]
+            dvelody[s0_] = -dvelody[face.s1_]
+            dvelodz[s0_] = -dvelodz[face.s1_]
+            # negate temp and species gradient (so gradient evaluates to zero on wall)
+            dTNdx[s0_] = -dTNdx[face.s1_]
+            dTNdy[s0_] = -dTNdy[face.s1_]
+            dTNdz[s0_] = -dTNdz[face.s1_]
 
 
 def adiabaticMovingWall(eos, blk, face, thtrdat, terms):
 
     nface = face.nface
+    p = blk.array["q"][:, :, :, 0]
+    u = blk.array["q"][:, :, :, 1]
+    v = blk.array["q"][:, :, :, 2]
+    w = blk.array["q"][:, :, :, 3]
+    TN = blk.array["q"][:, :, :, 4::]
 
     if terms == "euler":
-        p = blk.array["q"][:, :, :, 0]
-        p[face.s0_] = p[face.s1_]
+        for s0_ in face.s0_:
+            p[s0_] = p[face.s1_]
 
-        u = blk.array["q"][:, :, :, 1]
-        v = blk.array["q"][:, :, :, 2]
-        w = blk.array["q"][:, :, :, 3]
+            u[s0_] = 2.0 * face.bcVals["u"] - u[face.s1_]
+            v[s0_] = 2.0 * face.bcVals["v"] - v[face.s1_]
+            w[s0_] = 2.0 * face.bcVals["w"] - w[face.s1_]
 
-        u[face.s0_] = 2.0 * face.bcVals["u"] - u[face.s1_]
-        v[face.s0_] = 2.0 * face.bcVals["v"] - v[face.s1_]
-        w[face.s0_] = 2.0 * face.bcVals["w"] - w[face.s1_]
-
-        TN = blk.array["q"][:, :, :, 4::]
-        TN[face.s0_] = TN[face.s1_]
+            TN[face.s0_] = TN[face.s1_]
 
         # Update conservatives
         eos(blk, thtrdat, nface, "prims")
@@ -111,17 +118,20 @@ def adiabaticMovingWall(eos, blk, face, thtrdat, terms):
         dvelodx = blk.array["dqdx"][:, :, :, 1:4]
         dvelody = blk.array["dqdy"][:, :, :, 1:4]
         dvelodz = blk.array["dqdz"][:, :, :, 1:4]
-        dvelodx[face.s0_] = 2.0 * dvelodx[face.s1_] - dvelodx[face.s2_]
-        dvelody[face.s0_] = 2.0 * dvelody[face.s1_] - dvelody[face.s2_]
-        dvelodz[face.s0_] = 2.0 * dvelodz[face.s1_] - dvelodz[face.s2_]
 
-        # negate temp and species gradient (so gradient evaluates to zero on wall)
         dTNdx = blk.array["dqdx"][:, :, :, 4::]
         dTNdy = blk.array["dqdy"][:, :, :, 4::]
         dTNdz = blk.array["dqdz"][:, :, :, 4::]
-        dTNdx[face.s0_] = -dTNdx[face.s1_]
-        dTNdy[face.s0_] = -dTNdy[face.s1_]
-        dTNdz[face.s0_] = -dTNdz[face.s1_]
+
+        for s0_, s2_ in zip(face.s0_, face.s2_):
+            dvelodx[s0_] = 2.0 * dvelodx[face.s1_] - dvelodx[s2_]
+            dvelody[s0_] = 2.0 * dvelody[face.s1_] - dvelody[s2_]
+            dvelodz[s0_] = 2.0 * dvelodz[face.s1_] - dvelodz[s2_]
+
+            # negate temp and species gradient (so gradient evaluates to zero on wall)
+            dTNdx[s0_] = -dTNdx[face.s1_]
+            dTNdy[s0_] = -dTNdy[face.s1_]
+            dTNdz[s0_] = -dTNdz[face.s1_]
 
 
 def isoTMovingWall(eos, blk, face, thtrdat, terms):
@@ -130,39 +140,42 @@ def isoTMovingWall(eos, blk, face, thtrdat, terms):
 
     if terms == "euler":
         p = blk.array["q"][:, :, :, 0]
-        p[face.s0_] = p[face.s1_]
-
         u = blk.array["q"][:, :, :, 1]
         v = blk.array["q"][:, :, :, 2]
         w = blk.array["q"][:, :, :, 3]
-
-        u[face.s0_] = 2.0 * face.bcVals["u"] - u[face.s1_]
-        v[face.s0_] = 2.0 * face.bcVals["v"] - v[face.s1_]
-        w[face.s0_] = 2.0 * face.bcVals["w"] - w[face.s1_]
-
-        # Match species
         N = blk.array["q"][:, :, :, 4::]
-        N[face.s0_] = N[face.s1_]
-
-        # Set tempeerature
         T = blk.array["q"][:, :, :, 4]
-        T[face.s0_] = 2.0 * face.bcVals["T"] - T[face.s1_]
+
+        for s0_ in face.s0_:
+            p[s0_] = p[face.s1_]
+
+            u[s0_] = 2.0 * face.bcVals["u"] - u[face.s1_]
+            v[s0_] = 2.0 * face.bcVals["v"] - v[face.s1_]
+            w[s0_] = 2.0 * face.bcVals["w"] - w[face.s1_]
+
+            # Match species
+            N[s0_] = N[face.s1_]
+
+            # Set tempeerature
+            T[s0_] = 2.0 * face.bcVals["T"] - T[face.s1_]
 
         # Update conservatives
         eos(blk, thtrdat, nface, "prims")
 
     elif terms == "viscous":
-        # extrapolate velocity gradient
         dvelodx = blk.array["dqdx"][:, :, :, 1:4]
         dvelody = blk.array["dqdy"][:, :, :, 1:4]
         dvelodz = blk.array["dqdz"][:, :, :, 1:4]
-        dvelodx[face.s0_] = 2.0 * dvelodx[face.s1_] - dvelodx[face.s2_]
-        dvelody[face.s0_] = 2.0 * dvelody[face.s1_] - dvelody[face.s2_]
-        dvelodz[face.s0_] = 2.0 * dvelodz[face.s1_] - dvelodz[face.s2_]
-        # extrapolate temp and species gradient
         dTNdx = blk.array["dqdx"][:, :, :, 4::]
         dTNdy = blk.array["dqdy"][:, :, :, 4::]
         dTNdz = blk.array["dqdz"][:, :, :, 4::]
-        dTNdx[face.s0_] = 2.0 * dTNdx[face.s1_] - dTNdx[face.s2_]
-        dTNdy[face.s0_] = 2.0 * dTNdy[face.s1_] - dTNdy[face.s2_]
-        dTNdz[face.s0_] = 2.0 * dTNdz[face.s1_] - dTNdz[face.s2_]
+
+        for s0_, s2_ in zip(face.s0_, face.s2_):
+            # extrapolate velocity gradient
+            dvelodx[s0_] = 2.0 * dvelodx[face.s1_] - dvelodx[s2_]
+            dvelody[s0_] = 2.0 * dvelody[face.s1_] - dvelody[s2_]
+            dvelodz[s0_] = 2.0 * dvelodz[face.s1_] - dvelodz[s2_]
+            # extrapolate temp and species gradient
+            dTNdx[s0_] = 2.0 * dTNdx[face.s1_] - dTNdx[s2_]
+            dTNdy[s0_] = 2.0 * dTNdy[face.s1_] - dTNdy[s2_]
+            dTNdz[s0_] = 2.0 * dTNdz[face.s1_] - dTNdz[s2_]
