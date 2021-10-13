98ac63daa2c67dce4cda7fac8e9cbc52e6f2d904
Kyle A. Schau <kaschau89@gmail.com>
2021-10-13 16:22:27 -0400

    Fixed face and cc ranges.

--- src/compute/diffFlux/diffusiveFlux.cpp
+++ src/compute/diffFlux/diffusiveFlux.cpp
@@ -8,7 +8,7 @@ void diffusiveFlux(block_ b, const thtrdat_ th) {
 //-------------------------------------------------------------------------------------------|
 // i flux face range
 //-------------------------------------------------------------------------------------------|
-  MDRange3 range_i({b.ng,b.ng,1},{b.ni+2*b.ng-1,b.nj+2*b.ng-2,b.nk+2*b.ng-2});
+  MDRange3 range_i({b.ng,b.ng,b.ng},{b.ni+b.ng, b.nj+b.ng-1, b.nk+b.ng-1});
   Kokkos::parallel_for("i face visc fluxes", range_i, KOKKOS_LAMBDA(const int i,
                                                                     const int j,
                                                                     const int k) {
@@ -114,7 +114,7 @@ void diffusiveFlux(block_ b, const thtrdat_ th) {
 //-------------------------------------------------------------------------------------------|
 // j flux face range
 //-------------------------------------------------------------------------------------------|
-  MDRange3 range_j({b.ng,b.ng,b.ng}, {b.ni+2*b.ng-2,b.nj+2*b.ng-1,b.nk+2*b.ng-2});
+  MDRange3 range_j({b.ng,b.ng,b.ng},{b.ni+b.ng-1, b.nj+b.ng, b.nk+b.ng-1});
   Kokkos::parallel_for("j face visc fluxes", range_j, KOKKOS_LAMBDA(const int i,
                                                                     const int j,
                                                                     const int k) {
@@ -220,7 +220,7 @@ void diffusiveFlux(block_ b, const thtrdat_ th) {
 //-------------------------------------------------------------------------------------------|
 // k flux face range
 //-------------------------------------------------------------------------------------------|
-  MDRange3 range_k({b.ng,b.ng,b.ng},{b.ni+2*b.ng-2,b.nj+2*b.ng-2,b.nk+2*b.ng-1});
+  MDRange3 range_k({b.ng,b.ng,b.ng},{b.ni+b.ng-1, b.nj+b.ng-1, b.nk+b.ng});
   Kokkos::parallel_for("k face visc fluxes", range_k, KOKKOS_LAMBDA(const int i,
                                                                     const int j,
                                                                     const int k) {
