e378e3aacd3da61af30bc94d827a86da2085507a
Kyle A. Schau <kaschau89@gmail.com>
2021-10-15 10:59:46 -0400

    Added k flux for 4 KEEP

--- src/compute/advFlux/fourthOrderKEEP.cpp
+++ src/compute/advFlux/fourthOrderKEEP.cpp
@@ -306,7 +306,7 @@ void fourthOrderKEEP(block_ b, const thtrdat_ th, const double primary) {
         rhoY += a*temprhoY;
       }
       rhoY *=2.0;
-      b.iF(i,j,k,5+n) = rhoY * V;
+      b.jF(i,j,k,5+n) = rhoY * V;
     }
 
   });
@@ -319,6 +319,148 @@ void fourthOrderKEEP(block_ b, const thtrdat_ th, const double primary) {
                                                                     const int j,
                                                                     const int k) {
 
+    const int order = 4;
+    const std::array<double, 2> aq = {2.0/3.0, -1.0/12.0};
+
+    double W;
+    double uf=0.0;
+    double vf=0.0;
+    double wf=0.0;
+
+    double a;
+    // Compute face normal volume flux vector
+    double tempu=0.0;
+    double tempv=0.0;
+    double tempw=0.0;
+    for (int is=1; is <= order/2; is++) {
+      a = aq[is-1];
+      tempu = 0.0;
+      tempv = 0.0;
+      tempw = 0.0;
+      for (int js=0; js <= is-1; js++) {
+        tempu += 0.5*( b.q(i,j,k+js,1) + b.q(i,j,k+js-is,1) );
+        tempv += 0.5*( b.q(i,j,k+js,2) + b.q(i,j,k+js-is,2) );
+        tempw += 0.5*( b.q(i,j,k+js,3) + b.q(i,j,k+js-is,3) );
+      }
+      uf += a*tempu;
+      vf += a*tempv;
+      wf += a*tempw;
+    }
+    uf *=2.0;
+    vf *=2.0;
+    wf *=2.0;
+
+    W = b.ksx(i,j,k)*uf +
+        b.ksy(i,j,k)*vf +
+        b.ksz(i,j,k)*wf ;
+
+    //Compute fluxes
+
+    // Continuity rho*Ui
+    double rho = 0.0;
+    double temprho = 0.0;
+    for (int is=1; is <= order/2; is++) {
+      a = aq[is-1];
+      temprho = 0.0;
+      for (int js=0; js <= is-1; js++) {
+        temprho += 0.5*( b.Q(i,j,k+js,0) + b.Q(i,j,k+js-is,0) );
+      }
+      rho += a*temprho;
+    }
+    rho *=2.0;
+
+    b.kF(i,j,k,0) = rho * W;
+
+    // x momentum rho*u*Ui+ p*Ax
+    // y momentum rho*v*Ui+ p*Ay
+    // w momentum rho*w*Ui+ p*Az
+    double rhou=0.0;
+    double rhov=0.0;
+    double rhow=0.0;
+    double p=0.0;
+    double temprhou, temprhov, temprhow, tempp;
+    for (int is=1; is <= order/2; is++) {
+      a = aq[is-1];
+      temprhou = 0.0;
+      temprhov = 0.0;
+      temprhow = 0.0;
+      tempp = 0.0;
+      for (int js=0; js <= is-1; js++) {
+        temprhou += 0.5*( b.Q(i,j,k+js,0) + b.Q(i,j,k+js-is,0) ) * 0.5*( b.q(i,j,k+js,1) + b.q(i,j,k+js-is,1) );
+        temprhov += 0.5*( b.Q(i,j,k+js,0) + b.Q(i,j,k+js-is,0) ) * 0.5*( b.q(i,j,k+js,2) + b.q(i,j,k+js-is,2) );
+        temprhow += 0.5*( b.Q(i,j,k+js,0) + b.Q(i,j,k+js-is,0) ) * 0.5*( b.q(i,j,k+js,3) + b.q(i,j,k+js-is,3) );
+        tempp    += 0.5*( b.q(i,j,k+js,0) + b.q(i,j,k+js-is,0) );
+      }
+      rhou += a*temprhou;
+      rhov += a*temprhov;
+      rhow += a*temprhow;
+      p += a*tempp;
+    }
+    rhow *=2.0;
+    rhov *=2.0;
+    rhou *=2.0;
+    p *=2.0;
+
+    b.kF(i,j,k,1) = rhou * W + p*b.ksx(i,j,k) ;
+
+    b.kF(i,j,k,2) = rhov * W + p*b.ksy(i,j,k) ;
+
+    b.kF(i,j,k,3) = rhow * W + p*b.ksz(i,j,k) ;
+
+    // Total energy (rhoE+ p)*Ui)
+    double rhoE=0.0;
+    double pu=0.0;
+    double temprhoE, temppu;
+    double e,em;
+    for (int is=1; is <= order/2; is++) {
+      a = aq[is-1];
+      temprhoE = 0.0;
+      temppu = 0.0;
+      for (int js=0; js <= is-1; js++) {
+        e = b.qh(i,j,k+js   ,4)/b.Q(i,j,k+js   ,0);
+        em= b.qh(i,j,k+js-is,4)/b.Q(i,j,k+js-is,0);
+
+        temprhoE += 0.5* ( b.Q(i,j,k+js,0) + b.Q(i,j,k+js-is,0) )
+                       * ( 0.5*(  e + em )
+                         + 0.5*(  b.q(i,j,k+js,1)*b.q(i,j,k+js-is,1)
+                                + b.q(i,j,k+js,2)*b.q(i,j,k+js-is,2)
+                                + b.q(i,j,k+js,3)*b.q(i,j,k+js-is,3) )
+                         );
+
+        temppu += 0.5*(
+                        b.q(i,j,k+js-is,0)*(  b.q(i,j,k+js   ,1)*b.ksx(i,j,k)
+                                            + b.q(i,j,k+js   ,2)*b.ksy(i,j,k)
+                                            + b.q(i,j,k+js   ,3)*b.ksz(i,j,k) ) +
+                        b.q(i,j,k+js   ,0)*(  b.q(i,j,k+js-is,1)*b.ksx(i,j,k)
+                                            + b.q(i,j,k+js-is,2)*b.ksy(i,j,k)
+                                            + b.q(i,j,k+js-is,3)*b.ksz(i,j,k) )
+                       );
+      }
+      rhoE += a*temprhoE;
+      pu   += a*temppu;
+    }
+    rhoE *=2.0;
+    pu   *=2.0;
+
+    b.kF(i,j,k,4) = rhoE * W + pu;
+
+    // Species
+    for (int n=0; n<th.ns-1; n++)
+    {
+
+      double rhoY = 0.0;
+      double temprhoY = 0.0;
+      for (int is=1; is <= order/2; is++) {
+        a = aq[is-1];
+        temprhoY = 0.0;
+        for (int js=0; js <= is-1; js++) {
+          temprhoY += 0.5*( (b.Q(i,j,k+js,0) + b.Q(i,j,k+js-is,0) ) * 0.5*(b.q(i,j,k+js,5+n)+b.q(i,j,k+js-is,5+n) ));
+        }
+        rhoY += a*temprhoY;
+      }
+      rhoY *=2.0;
+      b.kF(i,j,k,5+n) = rhoY * W;
+    }
 
   });
 
