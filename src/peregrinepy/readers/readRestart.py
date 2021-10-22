87028c8dc7c156e2d81a6b6c9973afbcdf710e38
Kyle Schau <kschau@gatech.edu>
2021-10-22 13:21:14 -0400

    Can now read in restarts with fewer species than expecting.
    Also fixed setting of imp and exp chemistry.

--- src/peregrinepy/readers/readRestart.py
+++ src/peregrinepy/readers/readRestart.py
@@ -43,9 +43,13 @@ def readRestart(mb, path="./", nrt=0, animate=True):
             blk.tme = list(f["iter"]["tme"])[0]
 
             for i, var in enumerate(variables):
-                blk.array["q"][readS + tuple([i])] = np.array(
-                    f["results"][var]
-                ).reshape((blk.ni - 1, blk.nj - 1, blk.nk - 1), order="F")
+                try:
+                    blk.array["q"][readS + tuple([i])] = np.array(
+                        f["results"][var]
+                    ).reshape((blk.ni - 1, blk.nj - 1, blk.nk - 1), order="F")
+                except KeyError:
+                    if blk.nblki == 0:
+                        print(f"Warning, {var} not found in restart. Leaving as is.")
 
     # Set the mb values as well
     mb.nrt = mb[0].nrt
