f4b0e7a4422421cc91ea529b7f05a90407002209
Kyle A. Schau <kaschau89@gmail.com>
2021-12-03 12:24:20 -0500

    Test

--- src/compute/utils/dQzero.cpp
+++ src/compute/utils/dQzero.cpp
@@ -11,30 +11,33 @@ void dQzero(std::vector<block_> mb) {
   //-------------------------------------------------------------------------------------------|
   int nblks = mb.size();
 
+  Kokkos::View<block_ *, exec_space> mbv("test", nblks);
+  for (int b = 0; b < nblks; b++) {
+    mbv(b) = mb[b];
+  }
+
   policy p(nblks, Kokkos::AUTO());
   Kokkos::parallel_for(
       "test", p, KOKKOS_LAMBDA(policy::member_type member) {
         int nblki = member.league_rank();
 
-        const int ni = mb[nblki].ni;
-        const int nj = mb[nblki].nj;
-        const int nk = mb[nblki].nk;
-        const int nl = mb[nblki].ne;
+        const int ni = mbv(nblki).ni;
+        const int nj = mbv(nblki).nj;
+        const int nk = mbv(nblki).nk;
+        const int nl = mbv(nblki).ne;
+        const int ng = mbv(nblki).ng;
 
         int nijkl = (ni - 1) * (nj - 1) * (nk - 1) * nl;
 
         Kokkos::parallel_for(
             Kokkos::TeamVectorRange(member, 0, nijkl), [=](const int &ijkl) {
-
-              const int i =
-                  mb[nblki].ng + ijkl / ((nj - 1) * (nk - 1) * nl);
+              const int i = ng + ijkl / ((nj - 1) * (nk - 1) * nl);
               const int j =
-                  mb[nblki].ng + (ijkl % ((nj - 1) * (nk - 1) * nl)) /
-                                     ((nk - 1) * nl);
-              const int k = mb[nblki].ng + (ijkl % ((nk - 1) * nl)) / nl;
+                  ng + (ijkl % ((nj - 1) * (nk - 1) * nl)) / ((nk - 1) * nl);
+              const int k = ng + (ijkl % ((nk - 1) * nl)) / nl;
               const int l = ijkl % nl;
 
-              mb[nblki].dQ(i, j, k, l) = 0.0;
+              mbv(nblki).dQ(i, j, k, l) = 0.0;
             });
       });
 }
