10636855905c1b72c7eeb98634a80772d421998b
Kyle A. Schau <kaschau89@gmail.com>
2021-12-03 11:56:36 -0500

    Testing manual parallel

--- src/compute/utils/dQzero.cpp
+++ src/compute/utils/dQzero.cpp
@@ -1,6 +1,7 @@
 #include "Kokkos_Core.hpp"
 #include "block_.hpp"
 #include "kokkos_types.hpp"
+#include <Kokkos_Macros.hpp>
 #include <vector>
 
 void dQzero(std::vector<block_> mb) {
@@ -16,14 +17,17 @@ void dQzero(std::vector<block_> mb) {
         int nblki = member.league_rank();
         block_ b = mb[nblki];
 
-        MDRange4 range_cc(
-            {b.ng, b.ng, b.ng, 0},
-            {b.ni + b.ng - 1, b.nj + b.ng - 1, b.nk + b.ng - 1, b.ne});
+        int nijkl = (b.ni-1)*(b.nj-1)*(b.nk-1)*b.ne;
 
-        Kokkos::parallel_for(
-            "Apply current fluxes to RHS", range_cc,
-            KOKKOS_LAMBDA(const int i, const int j, const int k, const int l) {
-              b.dQ(i, j, k, l) = 0.0;
+        Kokkos::parallel_for(Kokkos::TeamVectorRange(member, 0, nijkl), KOKKOS_LAMBDA(const int& ijkl){
+
+            const int i = b.ng + ijkl/((b.nj-1)*(b.nk-1)*b.ne);
+            const int j = b.ng + (ijkl%((b.nj-1)*(b.nk-1)*b.ne))/((b.nk-1)*b.ne);
+            const int k = b.ng + (ijkl%((b.nk-1)*b.ne))/b.ne;
+            const int l = ijkl%b.ne;
+
+            b.dQ(i, j, k, l) = 0.0;
+
             });
       });
 }
