bfcec807d389d60a456e10ae5e262c502e504fad
Kyle A. Schau <kaschau89@gmail.com>
2021-12-06 08:06:37 -0500

    Dont need paraview simple

--- src/peregrinepy/coproc/coprocessor.py
+++ src/peregrinepy/coproc/coprocessor.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# import paraview.simple
 from paraview.modules import vtkPVCatalyst as catalyst
 from paraview.modules import vtkPVPythonCatalyst as pythoncatalyst
 import vtk
