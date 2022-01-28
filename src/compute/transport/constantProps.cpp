29286e2513e4b138a5ee609e03c6f2c2c564e9ba
Kyle A. Schau <kaschau89@gmail.com>
2022-01-28 11:45:23 -0500

    It isnt exactly beautiful, but with some simple macros we can now
    make ns a compile time parameter. Fixes #98

--- src/compute/transport/constantProps.cpp
+++ src/compute/transport/constantProps.cpp
@@ -12,48 +12,66 @@ void constantProps(block_ b,
              const int indxJ/*=0*/,
              const int indxK/*=0*/) {
 
-  MDRange3 range = get_range3(b, nface, indxI, indxJ, indxK);
+#ifdef NSCOMPILE
+  const int ns=NS;
+#else
   Kokkos::Experimental::UniqueToken<exec_space> token;
   int numIds = token.size();
-
   const int ns=th.ns;
   twoDview Y("Y", ns, numIds);
   twoDview X("X", ns, numIds);
+#endif
 
+#ifdef NSCOMPILE
+  #define Y(INDEX) Y[INDEX]
+  #define X(INDEX) X[INDEX]
+  #define ns NS
+#else
+  #define Y(INDEX) Y(INDEX,id)
+  #define X(INDEX) X(INDEX,id)
+#endif
+
+  MDRange3 range = get_range3(b, nface, indxI, indxJ, indxK);
   Kokkos::parallel_for("Const Props Transport",
                        range,
                        KOKKOS_LAMBDA(const int i,
                                      const int j,
                                      const int k) {
+#ifndef NSCOMPILE
   int id = token.acquire();
+#endif
 
+#ifdef NSCOMPILE
+  double Y(ns);
+  double X(ns);
+#endif
   double MWmix;
 
   // Compute nth species Y
-  Y(ns-1,id) = 1.0;
+  Y(ns-1) = 1.0;
   for (int n=0; n<ns-1; n++)
   {
-    Y(n,id) = b.q(i,j,k,5+n);
-    Y(ns-1,id) -= Y(n,id);
+    Y(n) = b.q(i,j,k,5+n);
+    Y(ns-1) -= Y(n);
   }
-  Y(ns-1,id) = fmax(0.0,Y(ns-1,id));
+  Y(ns-1) = fmax(0.0,Y(ns-1));
 
   // Update mixture properties
   // Mole fractions
   double mass=0.0;
   for (int n=0; n<=ns-1; n++)
   {
-    mass += Y(n,id)/th.MW(n);
+    mass += Y(n)/th.MW(n);
   }
   for (int n=0; n<=ns-1; n++)
   {
-    X(n,id) = Y(n,id)/th.MW(n)/mass;
+    X(n) = Y(n)/th.MW(n)/mass;
   }
   // Mean molecular weight
   MWmix = 0.0;
   for (int n=0; n<=ns-1; n++)
   {
-    MWmix += X(n,id)*th.MW(n);
+    MWmix += X(n)*th.MW(n);
   }
 
   // viscosity mixture
@@ -67,9 +85,9 @@ void constantProps(block_ b,
     {
       phi =  pow((1.0 + sqrt(th.mu0(n)/th.mu0(n2)*sqrt(th.MW(n2)/th.MW(n)))),2.0) /
                 ( sqrt(8.0)*sqrt(1+th.MW(n)/th.MW(n2)));
-      phitemp += phi*X(n2,id);
+      phitemp += phi*X(n2);
     }
-    mu += th.mu0(n)*X(n,id)/phitemp;
+    mu += th.mu0(n)*X(n)/phitemp;
   }
 
   // thermal conductivity mixture
@@ -79,8 +97,8 @@ void constantProps(block_ b,
   double sum2=0.0;
   for (int n=0; n<=ns-1; n++)
   {
-    sum1 += X(n,id) * th.kappa0(n);
-    sum2 += X(n,id) / th.kappa0(n);
+    sum1 += X(n) * th.kappa0(n);
+    sum2 += X(n) / th.kappa0(n);
   }
   kappa = 0.5*(sum1+1.0/sum2);
 
@@ -96,6 +114,8 @@ void constantProps(block_ b,
     b.qt(i,j,k,2+n) = kappa / ( b.Q(i,j,k,0) * b.qh(i,j,k,1) );
   }
 
+#ifndef NSCOMPILE
   token.release(id);
+#endif
   });
 }
