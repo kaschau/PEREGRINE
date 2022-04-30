5e0e3cf19ec49d7da8f8999d4605719bf678c67f
Kyle Schau <kaschau89@gmail.com>
2022-04-30 10:27:44 -0400

    Added max dt for variable time step

--- src/peregrinepy/mpiComm/mpiUtils.py
+++ src/peregrinepy/mpiComm/mpiUtils.py
@@ -58,7 +58,8 @@ def getDtMaxCFL(mb):
 
     if mb.config["simulation"]["variableTimeStep"]:
         cflMAX = mb.config["simulation"]["maxCFL"]
-        dt = cflMAX / cfl[2]
+        dt = min(cflMAX / cfl[2], mb.config["simulation"]["maxDt"])
+
         mb.config["simulation"]["dt"] = dt
     else:
         dt = mb.config["simulation"]["dt"]
