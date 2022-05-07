54f91c0161ad9956ded565aa1c10ff905eb398fa
Kyle Schau <kaschau89@gmail.com>
2022-05-07 10:18:48 -0400

    Fixed check nan to actually make sense

--- src/compute/utils/checkNan.cpp
+++ src/compute/utils/checkNan.cpp
@@ -9,22 +9,25 @@ int checkNan(std::vector<block_> mb) {
   //-------------------------------------------------------------------------------------------|
   // Check for nans and infs in the solution array.
   //-------------------------------------------------------------------------------------------|
-  int foundFinite;
-  int allFinite = 1; // <-- allFinite stays 1 when non nans are detected
+  int allFinite;
+  int foundNan = 0; // <-- foundNan ends up as q when nans are detected
 
   for (const block_ b : mb) {
     MDRange4 range_cc({b.ng, b.ng, b.ng, 0}, {b.ni + b.ng - 1, b.nj + b.ng - 1,
                                               b.nk + b.ng - 1, b.ne});
     Kokkos::parallel_reduce(
         "check nan", range_cc,
-        KOKKOS_LAMBDA(const int i, const int j, const int k, const int l, int &finite) {
+        KOKKOS_LAMBDA(const int i, const int j, const int k, const int l,
+                      int &finite) {
           finite = fmin(isfinite(b.Q(i, j, k, l)), finite);
         },
-        Kokkos::Min<int>(foundFinite));
+        Kokkos::Min<int>(allFinite));
 
-    allFinite = fmin(allFinite, 1 - std::min(allFinite, foundFinite));
+    if (allFinite == 0){
+        foundNan = 1;
+        break;
+    }
   }
 
-
-  return allFinite;
+  return foundNan;
 }
