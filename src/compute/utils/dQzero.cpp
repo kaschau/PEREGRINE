175e4acd6a6f9edd64a23144445eaab82d12fea5
Kyle Schau <kaschau89@gmail.com>
2022-05-14 13:34:50 -0400

    Replaced som parallel for with deep copy. Added A = Const

--- src/compute/utils/dQzero.cpp
+++ src/compute/utils/dQzero.cpp
@@ -7,19 +7,5 @@ void dQzero(block_ b) {
 //-------------------------------------------------------------------------------------------|
 // Zero out dQ
 //-------------------------------------------------------------------------------------------|
-  MDRange4 range_cc({b.ng, b.ng, b.ng,0},{b.ni+b.ng-1,
-                                          b.nj+b.ng-1,
-                                          b.nk+b.ng-1,
-                                          b.ne});
-  Kokkos::parallel_for("Zero out dQ",
-                       range_cc,
-                       KOKKOS_LAMBDA(const int i,
-                                     const int j,
-                                     const int k,
-                                     const int l) {
-
-    b.dQ(i,j,k,l) = 0.0;
-
-  });
-
+  Kokkos::deep_copy(b.dQ, 0.0);
 }
