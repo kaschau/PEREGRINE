d6652e9c97548397400684113cbef9e1f8d38c17
Kyle A. Schau <kaschau89@gmail.com>
2022-06-01 16:20:16 -0400

    We cant apply strict bcs to secondary flux when artificial diss
    is being used.

--- src/peregrinepy/RHS.py
+++ src/peregrinepy/RHS.py
@@ -16,9 +16,6 @@ def RHS(mb):
 
         # Secondary advective fluxes
         mb.secondaryAdvFlux(blk, mb.thtrdat)
-        # Apply strict advective boundary conditions
-        for face in blk.faces:
-            face.bcFunc(blk, face, mb.eos, mb.thtrdat, "strict", mb.tme)
         mb.applySecondaryAdvFlux(blk, 0.0)  # <-- 0.0 is for secondary flux
 
         # Diffusive fluxes
