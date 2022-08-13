bd8c22027c0526a5d9ad9760cc5ec8f3aa681d1d
Kyle Schau <kaschau89@gmail.com>
2022-08-13 13:21:27 -0400

    First try at a better structure for grid writers.
    Added ability to pass lump argument for writing single files.

--- src/peregrinepy/writers/writeGrid.py
+++ src/peregrinepy/writers/writeGrid.py
@@ -6,7 +6,66 @@ from copy import deepcopy
 from ..misc import progressBar
 
 
-def writeGrid(mb, path="./", precision="double", withHalo=False):
+class gridXdmf:
+    def __init__(self, path, precision, lump):
+        self.lump = lump
+        self.path = path
+
+        # This is the main xdmf object
+        self.tree = etree.Element("Xdmf")
+        self.tree.set("Version", "2")
+
+        self.domainElem = etree.SubElement(self.tree, "Domain")
+        self.gridElem = etree.SubElement(self.domainElem, "Grid")
+        self.gridElem.set("Name", "PEREGRINE Output")
+        self.gridElem.set("GridType", "Collection")
+        self.gridElem.set("CollectionType", "Spatial")
+
+        # This is a template of an individual block
+        self.blockTemplate = etree.Element("Grid")
+        self.blockTemplate.set("Name", "B#Here")
+
+        topologyElem = etree.SubElement(self.blockTemplate, "Topology")
+        topologyElem.set("TopologyType", "3DSMesh")
+        topologyElem.set("NumberOfElements", "Num Elem Here")
+        geometryElem = etree.SubElement(self.blockTemplate, "Geometry")
+        geometryElem.set("GeometryType", "X_Y_Z")
+
+        dataXElem = etree.SubElement(geometryElem, "DataItem")
+        dataXElem.set("NumberType", "Float")
+        dataXElem.set("Dimensions", "XYZ Dims Here")
+        dataXElem.set("Precision", "8" if precision == "double" else "4")
+        dataXElem.set("Format", "HDF")
+
+        dataXElem.text = "gridFile location:/coordinate/x"
+        geometryElem.append(deepcopy(dataXElem))
+        geometryElem[-1].text = "gridFile location:/coordinates/y"
+
+        geometryElem.append(deepcopy(dataXElem))
+        geometryElem[-1].text = "gridFile location:/coordinates/z"
+
+    def addBlock(self, nblki, ni, nj, nk, ng):
+
+        block = deepcopy(self.blockTemplate)
+        block.set("Name", f"B{nblki:06d}")
+        topo = block.find("Topology")
+        topo.set("NumberOfElements", f"{nk+2*ng} {nj+2*ng} {ni+2*ng}")
+
+        for coord, i in zip(["x", "y", "z"], [0, 1, 2]):
+            X = block.find("Geometry")[i]
+            X.set("Dimensions", f"{nk+2*ng} {nj+2*ng} {ni+2*ng}")
+            X.text = self.getGridFileLocation(nblki) + coord
+
+        self.gridElem.append(deepcopy(block))
+
+    def getGridFileLocation(self, nblki):
+        if self.lump:
+            return f"./grid.h5:/coordinates_{nblki:06d}/"
+        else:
+            return f"./g.{nblki:06d}.h5:/coordinates_{nblki:06d}/"
+
+
+def writeGrid(mb, path="./", precision="double", withHalo=False, lump=False):
     """This function produces an hdf5 file from a peregrinepy.multiBlock.grid (or a descendant) for viewing in Paraview.
     Parameters
     ----------
@@ -26,15 +85,12 @@ def writeGrid(mb, path="./", precision="double", withHalo=False):
     else:
         fdtype = "<f8"
 
-    xdmfElem = etree.Element("Xdmf")
-    xdmfElem.set("Version", "2")
+    # Start the xdmf tree
+    xdmfTree = gridXdmf(path, precision, lump)
 
-    domainElem = etree.SubElement(xdmfElem, "Domain")
-
-    gridElem = etree.SubElement(domainElem, "Grid")
-    gridElem.set("Name", "PEREGRINE Output")
-    gridElem.set("GridType", "Collection")
-    gridElem.set("CollectionType", "Spatial")
+    # If we are lumping the files, open it here
+    if lump:
+        f = h5py.File(f"{path}/grid.h5", "w")
 
     for blk in mb:
         if withHalo and blk.blockType == "solver":
@@ -50,63 +106,46 @@ def writeGrid(mb, path="./", precision="double", withHalo=False):
         else:
             writeS = np.s_[:, :, :]
 
-        with h5py.File(f"{path}/g.{blk.nblki:06d}.h5", "w") as f:
-            f.create_group("coordinates")
-            f.create_group("dimensions")
+        nblkiS = f"{blk.nblki:06d}"
+        coordS = "coordinates_" + nblkiS
+        dimS = "dimensions_" + nblkiS
 
-            f["dimensions"].create_dataset("ni", shape=(1,), dtype="int32")
-            f["dimensions"].create_dataset("nj", shape=(1,), dtype="int32")
-            f["dimensions"].create_dataset("nk", shape=(1,), dtype="int32")
+        # If we are doing serial output, open the file here
+        if not lump:
+            f = h5py.File(f"{path}/g.{nblkiS}.h5", "w")
 
-            dset = f["dimensions"]["ni"]
-            dset[0] = blk.ni + 2 * ng
-            dset = f["dimensions"]["nj"]
-            dset[0] = blk.nj + 2 * ng
-            dset = f["dimensions"]["nk"]
-            dset[0] = blk.nk + 2 * ng
+        f.create_group(coordS)
+        f.create_group(dimS)
 
-            extent = (blk.ni + 2 * ng) * (blk.nj + 2 * ng) * (blk.nk + 2 * ng)
-            f["coordinates"].create_dataset("x", shape=(extent,), dtype=fdtype)
-            f["coordinates"].create_dataset("y", shape=(extent,), dtype=fdtype)
-            f["coordinates"].create_dataset("z", shape=(extent,), dtype=fdtype)
+        f[dimS].create_dataset("ni", shape=(1,), dtype="int32")
+        f[dimS].create_dataset("nj", shape=(1,), dtype="int32")
+        f[dimS].create_dataset("nk", shape=(1,), dtype="int32")
 
-            dset = f["coordinates"]["x"]
-            dset[:] = blk.array["x"][writeS].ravel(order="F")
-            dset = f["coordinates"]["y"]
-            dset[:] = blk.array["y"][writeS].ravel(order="F")
-            dset = f["coordinates"]["z"]
-            dset[:] = blk.array["z"][writeS].ravel(order="F")
+        dset = f[dimS]["ni"]
+        dset[0] = blk.ni + 2 * ng
+        dset = f[dimS]["nj"]
+        dset[0] = blk.nj + 2 * ng
+        dset = f[dimS]["nk"]
+        dset[0] = blk.nk + 2 * ng
 
-        blockElem = etree.Element("Grid")
-        blockElem.set("Name", f"B{blk.nblki:06d}")
+        extent = (blk.ni + 2 * ng) * (blk.nj + 2 * ng) * (blk.nk + 2 * ng)
+        f[coordS].create_dataset("x", shape=(extent,), dtype=fdtype)
+        f[coordS].create_dataset("y", shape=(extent,), dtype=fdtype)
+        f[coordS].create_dataset("z", shape=(extent,), dtype=fdtype)
 
-        topologyElem = etree.SubElement(blockElem, "Topology")
-        topologyElem.set("TopologyType", "3DSMesh")
-        topologyElem.set(
-            "NumberOfElements", f"{blk.nk+2*ng} {blk.nj+2*ng} {blk.ni+2*ng}"
-        )
+        dset = f[coordS]["x"]
+        dset[:] = blk.array["x"][writeS].ravel(order="F")
+        dset = f[coordS]["y"]
+        dset[:] = blk.array["y"][writeS].ravel(order="F")
+        dset = f[coordS]["z"]
+        dset[:] = blk.array["z"][writeS].ravel(order="F")
 
-        geometryElem = etree.SubElement(blockElem, "Geometry")
-        geometryElem.set("GeometryType", "X_Y_Z")
-
-        dataX1Elem = etree.SubElement(geometryElem, "DataItem")
-        dataX1Elem.set("NumberType", "Float")
-        dataX1Elem.set("Dimensions", f"{blk.nk+2*ng} {blk.nj+2*ng} {blk.ni+2*ng}")
-        dataX1Elem.set("Precision", "8")
-        dataX1Elem.set("Format", "HDF")
-        dataX1Elem.text = f"g.{blk.nblki:06d}.h5:/coordinates/x"
-
-        geometryElem.append(deepcopy(dataX1Elem))
-        geometryElem[-1].text = f"g.{blk.nblki:06d}.h5:/coordinates/y"
-
-        geometryElem.append(deepcopy(dataX1Elem))
-        geometryElem[-1].text = f"g.{blk.nblki:06d}.h5:/coordinates/z"
-
-        gridElem.append(deepcopy(blockElem))
+        # Add block to xdmf tree
+        xdmfTree.addBlock(blk.nblki, blk.ni, blk.nj, blk.nk, ng)
 
         if mb.mbType in ["grid", "restart"]:
             progressBar(blk.nblki + 1, len(mb), f"Writing out gridBlock {blk.nblki}")
 
-    et = etree.ElementTree(xdmfElem)
+    et = etree.ElementTree(xdmfTree.tree)
     save_file = f"{path}/g.xmf"
     et.write(save_file, pretty_print=True, encoding="UTF-8", xml_declaration=True)
