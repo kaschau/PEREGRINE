81862b9166a51410def18d6653e81c3e700ebf78
Kyle A. Schau <kaschau89@gmail.com>
2022-08-13 13:21:27 -0400

    First try at a better structure for grid writers.
    Added ability to pass lump argument for writing single files.

--- src/peregrinepy/readers/readGrid.py
+++ src/peregrinepy/readers/readGrid.py
@@ -5,7 +5,7 @@ import numpy as np
 from ..misc import progressBar
 
 
-def readGrid(mb, path="./", justNi=False):
+def readGrid(mb, path="./", lump=False, justNi=False):
     """
     This function reads in all the HDF5 grid files in
     :path: and adds the coordinate data to a supplied
@@ -30,8 +30,11 @@ def readGrid(mb, path="./", justNi=False):
     if justNi:
         assert mb.mbType not in ["restart", "solver"]
 
+    # If were reading a lumped open the file here
+    if lump:
+        f = h5py.File(f"{path}/grid.h5", "r")
+
     for blk in mb:
-        fileName = f"{path}/g.{blk.nblki:06d}.h5"
         if blk.blockType == "solver":
             ng = blk.ng
             readS = np.s_[ng:-ng, ng:-ng, ng:-ng]
@@ -39,21 +42,29 @@ def readGrid(mb, path="./", justNi=False):
             ng = 0
             readS = np.s_[:, :, :]
 
-        with h5py.File(fileName, "r") as f:
-            ni = list(f["dimensions"]["ni"])[0]
-            nj = list(f["dimensions"]["nj"])[0]
-            nk = list(f["dimensions"]["nk"])[0]
+        # If were not reading a lumped file, open it here
+        if not lump:
+            fileName = f"{path}/g.{blk.nblki:06d}.h5"
+            f = h5py.File(fileName, "r")
 
-            blk.ni = int(ni)
-            blk.nj = int(nj)
-            blk.nk = int(nk)
+        nblkiS = f"{blk.nblki:06d}"
+        coordS = "coordinates_" + nblkiS
+        dimS = "dimensions_" + nblkiS
 
-            if not justNi:
-                blk.initGridArrays()
-                for name in ("x", "y", "z"):
-                    blk.array[name][readS] = np.array(f["coordinates"][name]).reshape(
-                        (ni, nj, nk), order="F"
-                    )
+        ni = list(f[dimS]["ni"])[0]
+        nj = list(f[dimS]["nj"])[0]
+        nk = list(f[dimS]["nk"])[0]
+
+        blk.ni = int(ni)
+        blk.nj = int(nj)
+        blk.nk = int(nk)
+
+        if not justNi:
+            blk.initGridArrays()
+            for name in ("x", "y", "z"):
+                blk.array[name][readS] = np.array(f[coordS][name]).reshape(
+                    (ni, nj, nk), order="F"
+                )
 
         if mb.mbType in ["grid", "restart"]:
             progressBar(blk.nblki + 1, len(mb), f"Reading in gridBlock {blk.nblki}")
