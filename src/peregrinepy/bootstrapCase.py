ff132174430f5ec11419493e682f0f798d9bae96
Kyle Schau <kaschau89@gmail.com>
2022-12-09 07:37:39 -0500

    For bc debugging purposes, just fill solver halos on readRestart

--- src/peregrinepy/bootstrapCase.py
+++ src/peregrinepy/bootstrapCase.py
@@ -171,7 +171,7 @@ def bootstrapCase(config):
     ################################################################
     # Generate conserved variables
     for blk in mb:
-        mb.eos(blk, mb.thtrdat, 0, "prims")
+        mb.eos(blk, mb.thtrdat, -1, "prims")
 
     # Consistify total flow field
     pg.consistify(mb)
