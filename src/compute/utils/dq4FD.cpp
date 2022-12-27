44b2c419d603190e1fc2d70948db8f2b38d5ca25
Kyle A. Schau <kaschau89@gmail.com>
2022-12-27 10:28:27 -0500

    Fixed bug in fourthorder FD derivative.

--- src/compute/utils/dq4FD.cpp
+++ src/compute/utils/dq4FD.cpp
@@ -16,11 +16,11 @@ void dq4FD(block_ &b) {
         double term1, term2, term3;
 
         term1 = -b.q(i + 2, j, k, l) + 8.0 * b.q(i + 1, j, k, l) -
-                8.0 * b.q(i - 1, j, k, l) + b.q(i + 2, j, k, l);
+                8.0 * b.q(i - 1, j, k, l) + b.q(i - 2, j, k, l);
         term2 = -b.q(i, j + 2, k, l) + 8.0 * b.q(i, j + 1, k, l) -
-                8.0 * b.q(i, j - 1, k, l) + b.q(i, j + 2, k, l);
+                8.0 * b.q(i, j - 1, k, l) + b.q(i, j - 2, k, l);
         term3 = -b.q(i, j, k + 2, l) + 8.0 * b.q(i, j, k + 1, l) -
-                8.0 * b.q(i, j, k - 1, l) + b.q(i, j, k + 2, l);
+                8.0 * b.q(i, j, k - 1, l) + b.q(i, j, k - 2, l);
 
         b.dqdx(i, j, k, l) =
             (term1 * b.dEdx(i, j, k) + term2 * b.dNdx(i, j, k) +
