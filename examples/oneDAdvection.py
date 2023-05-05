f4ad3caec3958b57ad0ff64137095afd7ec3036a
Kyle A. Schau <kaschau89@gmail.com>
2023-05-05 13:11:56 -0400

    Better plots with 1d advection

--- examples/oneDAdvection.py
+++ examples/oneDAdvection.py
@@ -17,12 +17,17 @@ import peregrinepy as pg
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use("~/.config/matplotlib/stylelib/whitePresentation.mplstyle")
+
+save = False
+
 
 def simulate(index="i"):
     config = pg.files.configFile()
-    config["RHS"]["shockHandling"] = "artificialDissipation"
-    config["RHS"]["secondaryAdvFlux"] = "KEEPdissipation"
-    config["RHS"]["switchAdvFlux"] = "vanLeer"
+    # config["RHS"]["primaryAdvFlux"] = "centralDifference"
+    # config["RHS"]["shockHandling"] = "artificialDissipation"
+    # config["RHS"]["secondaryAdvFlux"] = "scalarDissipation"
+    # config["RHS"]["switchAdvFlux"] = "jamesonPressure"
     config["RHS"]["diffusion"] = False
     config.validateConfig()
     mb = pg.multiBlock.generateMultiBlockSolver(1, config)
@@ -67,7 +72,7 @@ def simulate(index="i"):
 
     ng = blk.ng
     R = 287.002507
-    cp = 1000.0
+    cp = 1002.838449439523
     gamma = cp / (cp - R)
     ccArray = {"i": "xc", "j": "yc", "k": "zc"}
     uIndex = {"i": 1, "j": 2, "k": 3}
@@ -84,9 +89,10 @@ def simulate(index="i"):
     pg.consistify(mb)
 
     # entropy stuff
-    blk.array["s"][:] = blk.array["Q"][:, :, :, 0] * np.log(
-        blk.array["q"][:, :, :, 0] * blk.array["Q"][:, :, :, 0] ** (-gamma)
+    s = cp / gamma * np.log(blk.array["q"][:, :, :, 4]) - R * np.log(
+        blk.array["Q"][:, :, :, 0]
     )
+    blk.array["s"][:] = blk.array["Q"][:, :, :, 0] * s
     blk.updateDeviceView(["s"])
     pg.consistify(mb)
 
@@ -101,14 +107,14 @@ def simulate(index="i"):
 
         if mb.nrt % 10 == 0:
             dS = np.sum(
-                blk.array["Q"][ng:-ng, ng:-ng, ng:-ng, 0]
-                * np.log(
-                    blk.array["q"][ng:-ng, ng:-ng, ng:-ng, 0]
-                    * blk.array["Q"][ng:-ng, ng:-ng, ng:-ng, 0] ** (-gamma)
+                blk.array["Q"][ng:-ng, ng, ng, 0]
+                * (
+                    cp / gamma * np.log(blk.array["q"][ng:-ng, ng, ng, 4])
+                    - R * np.log(blk.array["Q"][ng:-ng, ng, ng, 0])
                 )
             )
             sDerived.append(dS)
-            eS = np.sum(blk.array["s"][ng:-ng, ng:-ng, ng:-ng])
+            eS = np.sum(blk.array["s"][ng:-ng, ng, ng])
             sEvolved.append(eS)
             t.append(mb.tme)
 
@@ -123,11 +129,12 @@ def simulate(index="i"):
     rho = blk.array["Q"][s_][:, 0]
     p = blk.array["q"][s_][:, 0]
     u = blk.array["q"][s_][:, uIndex[index]]
-    sd = rho * np.log(p * rho**-gamma)
+    T = blk.array["q"][s_][:, 4]
+    sd = rho * (cp / gamma * np.log(T) - R * np.log(rho))
     se = blk.array["s"][s_]
-    ax1.plot(x, rho, color="g", label="rho", linewidth=0.5)
-    ax1.plot(x, p, color="r", label="p", linewidth=0.5)
-    ax1.plot(x, u, color="k", label="u", linewidth=0.5)
+    ax1.plot(x, rho, color="g", label="rho")
+    ax1.plot(x, p, color="r", label="p")
+    ax1.plot(x, u, color="k", label="u")
     ax1.scatter(
         x,
         initial_rho[ng:-ng, ng:-ng, ng:-ng],
@@ -135,31 +142,49 @@ def simulate(index="i"):
         facecolor="w",
         edgecolor="b",
         label="exact",
-        linewidth=0.5,
     )
     ax1.legend()
+    if save:
+        fig.savefig("solution_NoDiss.png")
     plt.show()
     plt.clf()
-    # entropy
-    plt.plot(x, sd, color="k", label="sd", linewidth=0.5)
-    plt.plot(x, se, color="r", label="se", linewidth=0.5)
+
+    # entropy space
+    plt.plot(x, sd, color="k", label="Recon")
+    plt.plot(x, se, color="r", label=r"$\partial{\rho s}/\partial{t}$")
+    plt.ylabel(r"$\rho s \quad [ c_{v}\ln (T) - R \ln (\rho) ]$")
+    plt.xlabel(r"x")
     plt.legend()
+    if save:
+        plt.savefig("spatial_entropy_NoDiss.png")
     plt.show()
     plt.clf()
-    plt.plot(t, sDerived, label="Derived")
-    plt.scatter(t, sEvolved, marker="o", label="Evolved")
+
+    # entropy total
+    plt.plot(t, (sDerived[0] - sDerived) / sDerived[0], label="Recon")
+    plt.scatter(
+        t,
+        (sEvolved[0] - sEvolved) / sEvolved[0],
+        c="orange",
+        marker="o",
+        label=r"$\partial{\rho s}/\partial{t}$",
+        # s=1.0,
+    )
     plt.legend()
-    plt.title(r"$\Delta(\rho s) / (\rho_0 s_0)$")
+    plt.ylabel(r"$\Delta(\rho s) / {(\rho s)}_0$")
+    plt.xlabel(r"$x$")
+    # plt.ylim((-0.002, 0.015))
+    if save:
+        plt.savefig("total_entropy_NoDiss.png")
     plt.show()
     plt.close()
 
 
 if __name__ == "__main__":
     try:
-        a = pg.compute.pgkokkos.ScopeGuard()
-        # pg.compute.pgkokkos.initialize()
+        pg.compute.pgkokkos.initialize()
         simulate("i")
-        # pg.compute.pgkokkos.finalize()
+        pg.compute.pgkokkos.finalize()
 
     except Exception as e:
         import sys
