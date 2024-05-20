bfbe4d4a92bf2c96224e09d89311bef097908a1b
Kyle A. Schau <kaschau89@gmail.com>
2024-05-20 12:59:10 -0400

    Simplifying.

--- src/compute/advFlux/KEPaEC.cpp
+++ src/compute/advFlux/KEPaEC.cpp
@@ -13,43 +13,37 @@ void KEPaEC(block_ &b) {
   Kokkos::parallel_for(
       "2nd order KEEP i face conv fluxes", range_i,
       KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double U;
-        double uf;
-        double vf;
-        double wf;
-        double pf;
-
         // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i - 1, j, k, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i - 1, j, k, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i - 1, j, k, 3));
+        double uf = 0.5 * (b.q(i, j, k, 1) + b.q(i - 1, j, k, 1));
+        double vf = 0.5 * (b.q(i, j, k, 2) + b.q(i - 1, j, k, 2));
+        double wf = 0.5 * (b.q(i, j, k, 3) + b.q(i - 1, j, k, 3));
 
-        U = b.isx(i, j, k) * uf + b.isy(i, j, k) * vf + b.isz(i, j, k) * wf;
+        double U =
+            b.isx(i, j, k) * uf + b.isy(i, j, k) * vf + b.isz(i, j, k) * wf;
 
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i - 1, j, k, 0));
+        double pf = 0.5 * (b.q(i, j, k, 0) + b.q(i - 1, j, k, 0));
 
         // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i - 1, j, k, 0));
+        double rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i - 1, j, k, 0));
 
         // Continuity rho*Ui
-        b.iF(i, j, k, 0) = rho * U;
+        double C = rho * U;
+        b.iF(i, j, k, 0) = C;
 
         // x momentum rho*u*Ui+ p*Ax
-        b.iF(i, j, k, 1) = rho * uf * U + pf * b.isx(i, j, k);
+        b.iF(i, j, k, 1) = C * uf + pf * b.isx(i, j, k);
 
         // y momentum rho*v*Ui+ p*Ay
-        b.iF(i, j, k, 2) = rho * vf * U + pf * b.isy(i, j, k);
+        b.iF(i, j, k, 2) = C * vf + pf * b.isy(i, j, k);
 
         // w momentum rho*w*Ui+ p*Az
-        b.iF(i, j, k, 3) = rho * wf * U + pf * b.isz(i, j, k);
+        b.iF(i, j, k, 3) = C * wf + pf * b.isz(i, j, k);
 
         // Total energy (rhoE+ p)*Ui)
-        double Kj = rho * 0.5 *
+        double Kj = C * 0.5 *
                     (b.q(i, j, k, 1) * b.q(i - 1, j, k, 1) +
                      b.q(i, j, k, 2) * b.q(i - 1, j, k, 2) +
-                     b.q(i, j, k, 3) * b.q(i - 1, j, k, 3)) *
-                    U;
+                     b.q(i, j, k, 3) * b.q(i - 1, j, k, 3));
 
         double Pj =
             0.5 * (b.q(i - 1, j, k, 0) * (b.q(i, j, k, 1) * b.isx(i, j, k) +
@@ -60,18 +54,16 @@ void KEPaEC(block_ &b) {
                                       b.q(i - 1, j, k, 3) * b.isz(i, j, k)));
 
         // solve for internal energy flux
-        double &TR = b.q(i, j, k, 4);
-        double &eR = b.qh(i, j, k, 4);
-        double &TL = b.q(i - 1, j, k, 4);
-        double &eL = b.qh(i - 1, j, k, 4);
-        double Ij = (eL * TR + eR * TL) / (TL + TR) * U;
+        double eR = b.qh(i, j, k, 4) / b.Q(i, j, k, 0);
+        double eL = b.qh(i - 1, j, k, 4) / b.Q(i - 1, j, k, 0);
+        double Ij = 2.0 * (eL * eR) / (eL + eR) * C;
 
         b.iF(i, j, k, 4) = Ij + Kj + Pj;
 
         // Species
         for (int n = 0; n < b.ne - 5; n++) {
           b.iF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i - 1, j, k, 5 + n)) * U;
+              0.5 * (b.q(i, j, k, 5 + n) + b.q(i - 1, j, k, 5 + n)) * C;
         }
       });
 
@@ -83,43 +75,37 @@ void KEPaEC(block_ &b) {
   Kokkos::parallel_for(
       "2nd order KEEP j face conv fluxes", range_j,
       KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double V;
-        double uf;
-        double vf;
-        double wf;
-        double pf;
-
         // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j - 1, k, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j - 1, k, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j - 1, k, 3));
+        double uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j - 1, k, 1));
+        double vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j - 1, k, 2));
+        double wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j - 1, k, 3));
 
-        V = b.jsx(i, j, k) * uf + b.jsy(i, j, k) * vf + b.jsz(i, j, k) * wf;
+        double V =
+            b.jsx(i, j, k) * uf + b.jsy(i, j, k) * vf + b.jsz(i, j, k) * wf;
 
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j - 1, k, 0));
+        double pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j - 1, k, 0));
 
         // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j - 1, k, 0));
+        double rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j - 1, k, 0));
 
         // Continuity rho*Vj
-        b.jF(i, j, k, 0) = rho * V;
+        double C = rho * V;
+        b.jF(i, j, k, 0) = C;
 
         // x momentum rho*u*Vj+ pAx
-        b.jF(i, j, k, 1) = rho * uf * V + pf * b.jsx(i, j, k);
+        b.jF(i, j, k, 1) = C * uf + pf * b.jsx(i, j, k);
 
         // y momentum rho*v*Vj+ pAy
-        b.jF(i, j, k, 2) = rho * vf * V + pf * b.jsy(i, j, k);
+        b.jF(i, j, k, 2) = C * vf + pf * b.jsy(i, j, k);
 
         // w momentum rho*w*Vj+ pAz
-        b.jF(i, j, k, 3) = rho * wf * V + pf * b.jsz(i, j, k);
+        b.jF(i, j, k, 3) = C * wf + pf * b.jsz(i, j, k);
 
         // Total energy (rhoE+P)*Vj)
-        double Kj = rho * 0.5 *
+        double Kj = C * 0.5 *
                     (b.q(i, j, k, 1) * b.q(i, j - 1, k, 1) +
                      b.q(i, j, k, 2) * b.q(i, j - 1, k, 2) +
-                     b.q(i, j, k, 3) * b.q(i, j - 1, k, 3)) *
-                    V;
+                     b.q(i, j, k, 3) * b.q(i, j - 1, k, 3));
 
         double Pj =
             0.5 * (b.q(i, j - 1, k, 0) * (b.q(i, j, k, 1) * b.jsx(i, j, k) +
@@ -130,18 +116,16 @@ void KEPaEC(block_ &b) {
                                       b.q(i, j - 1, k, 3) * b.jsz(i, j, k)));
 
         // solve for internal energy flux
-        double &TR = b.q(i, j, k, 4);
-        double &eR = b.qh(i, j, k, 4);
-        double &TL = b.q(i, j - 1, k, 4);
-        double &eL = b.qh(i, j - 1, k, 4);
-        double Ij = (eL * TR + eR * TL) / (TL + TR) * V;
+        double eR = b.qh(i, j, k, 4) / b.Q(i, j, k, 0);
+        double eL = b.qh(i, j - 1, k, 4) / b.Q(i, j - 1, k, 0);
+        double Ij = 2.0 * (eL * eR) / (eL + eR) * C;
 
         b.jF(i, j, k, 4) = Ij + Kj + Pj;
 
         // Species
         for (int n = 0; n < b.ne - 5; n++) {
           b.jF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i, j - 1, k, 5 + n)) * V;
+              0.5 * (b.q(i, j, k, 5 + n) + b.q(i, j - 1, k, 5 + n)) * C;
         }
       });
 
@@ -153,42 +137,37 @@ void KEPaEC(block_ &b) {
   Kokkos::parallel_for(
       "2nd order KEEP k face conv fluxes", range_k,
       KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double W;
-        double uf;
-        double vf;
-        double wf;
-        double pf;
-
         // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j, k - 1, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j, k - 1, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j, k - 1, 3));
+        double uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j, k - 1, 1));
+        double vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j, k - 1, 2));
+        double wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j, k - 1, 3));
 
-        W = b.ksx(i, j, k) * uf + b.ksy(i, j, k) * vf + b.ksz(i, j, k) * wf;
+        double W =
+            b.ksx(i, j, k) * uf + b.ksy(i, j, k) * vf + b.ksz(i, j, k) * wf;
 
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j, k - 1, 0));
+        double pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j, k - 1, 0));
 
         // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j, k - 1, 0));
+        double rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j, k - 1, 0));
+
         // Continuity rho*Wk
-        b.kF(i, j, k, 0) = rho * W;
+        double C = rho * W;
+        b.kF(i, j, k, 0) = C;
 
         // x momentum rho*u*Wk+ pAx
-        b.kF(i, j, k, 1) = rho * uf * W + pf * b.ksx(i, j, k);
+        b.kF(i, j, k, 1) = C * uf + pf * b.ksx(i, j, k);
 
         // y momentum rho*v*Wk+ pAy
-        b.kF(i, j, k, 2) = rho * vf * W + pf * b.ksy(i, j, k);
+        b.kF(i, j, k, 2) = C * vf + pf * b.ksy(i, j, k);
 
         // w momentum rho*w*Wk+ pAz
-        b.kF(i, j, k, 3) = rho * wf * W + pf * b.ksz(i, j, k);
+        b.kF(i, j, k, 3) = C * wf + pf * b.ksz(i, j, k);
 
         // Total energy (rhoE+P)*Wk)
-        double Kj = rho * 0.5 *
+        double Kj = C * 0.5 *
                     (b.q(i, j, k, 1) * b.q(i, j, k - 1, 1) +
                      b.q(i, j, k, 2) * b.q(i, j, k - 1, 2) +
-                     b.q(i, j, k, 3) * b.q(i, j, k - 1, 3)) *
-                    W;
+                     b.q(i, j, k, 3) * b.q(i, j, k - 1, 3));
 
         double Pj =
             0.5 * (b.q(i, j, k - 1, 0) * (b.q(i, j, k, 1) * b.ksx(i, j, k) +
@@ -199,17 +178,15 @@ void KEPaEC(block_ &b) {
                                       b.q(i, j, k - 1, 3) * b.ksz(i, j, k)));
 
         // solve for internal energy flux
-        double &TR = b.q(i, j, k, 4);
-        double &eR = b.qh(i, j, k, 4);
-        double &TL = b.q(i, j, k - 1, 4);
-        double &eL = b.qh(i, j, k - 1, 4);
-        double Ij = (eL * TR + eR * TL) / (TL + TR) * W;
+        double eR = b.qh(i, j, k, 4) / b.Q(i, j, k, 0);
+        double eL = b.qh(i, j, k - 1, 4) / b.Q(i, j, k - 1, 0);
+        double Ij = (eL * eR) / (eL + eR) * C;
 
         b.kF(i, j, k, 4) = Ij + Kj + Pj;
         // Species
         for (int n = 0; n < b.ne - 5; n++) {
           b.kF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i, j, k - 1, 5 + n)) * W;
+              0.5 * (b.q(i, j, k, 5 + n) + b.q(i, j, k - 1, 5 + n)) * C;
         }
       });
 }
