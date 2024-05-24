fb415f25267b6ab1790e4bf5930a3d1a88b4f30f
Kyle A. Schau <kaschau89@gmail.com>
2024-05-24 09:05:04 -0400

    Catching up

--- src/compute/advFlux/centralDifference.cpp
+++ src/compute/advFlux/centralDifference.cpp
@@ -1,164 +1,77 @@
-#include "Kokkos_Core.hpp"
 #include "block_.hpp"
 #include "kokkosTypes.hpp"
 #include "thtrdat_.hpp"
+#include <Kokkos_Core.hpp>
 
-void centralDifference(block_ &b) {
+static void computeFlux(const block_ &b, fourDview &iF, const threeDview &isx,
+                        const threeDview &isy, const threeDview &isz,
+                        const int iMod, const int jMod, const int kMod) {
 
-  //-------------------------------------------------------------------------------------------|
-  // i flux face range
-  //-------------------------------------------------------------------------------------------|
-  MDRange3 range_i({b.ng, b.ng, b.ng},
-                   {b.ni + b.ng, b.nj + b.ng - 1, b.nk + b.ng - 1});
+  // face flux range
+  MDRange3 range(
+      {b.ng, b.ng, b.ng},
+      {b.ni + b.ng - 1 + iMod, b.nj + b.ng - 1 + jMod, b.nk + b.ng - 1 + kMod});
+
   Kokkos::parallel_for(
-      "2nd order central difference i face conv fluxes", range_i,
+      "2nd order central difference face conv fluxes", range,
       KOKKOS_LAMBDA(const int i, const int j, const int k) {
         // Compute face normal volume flux vector
         double uR = b.q(i, j, k, 1);
-        double uL = b.q(i - 1, j, k, 1);
+        double uL = b.q(i - iMod, j - jMod, k - kMod, 1);
+        double vR = b.q(i, j, k, 2);
+        double vL = b.q(i - iMod, j - jMod, k - kMod, 2);
+        double wR = b.q(i, j, k, 3);
+        double wL = b.q(i - iMod, j - jMod, k - kMod, 3);
 
-        double rhouR = b.Q(i, j, k, 1);
-        double rhouL = b.Q(i - 1, j, k, 1);
-
-        double a = b.isx(i, j, k);
+        double UfR = uR * isx(i, j, k) + vR * isy(i, j, k) + wR * isz(i, j, k);
+        double UfL = uL * isx(i, j, k) + vL * isy(i, j, k) + wL * isz(i, j, k);
 
         double pR = b.q(i, j, k, 0);
-        double pL = b.q(i - 1, j, k, 0);
-
+        double pL = b.q(i - iMod, j - jMod, k - kMod, 0);
         // Compute fluxes
 
         // Continuity rho*Ui
-        b.iF(i, j, k, 0) = 0.5 * (rhouR + rhouL) * a;
+        double rhouR = b.Q(i, j, k, 1);
+        double rhovR = b.Q(i, j, k, 2);
+        double rhowR = b.Q(i, j, k, 3);
+        double rhouL = b.Q(i - iMod, j - jMod, k - kMod, 1);
+        double rhovL = b.Q(i - iMod, j - jMod, k - kMod, 2);
+        double rhowL = b.Q(i - iMod, j - jMod, k - kMod, 3);
+        double CjR =
+            isx(i, j, k) * rhouR + isy(i, j, k) * rhovR + isz(i, j, k) * rhowR;
+        double CjL =
+            isx(i, j, k) * rhouL + isy(i, j, k) * rhovL + isz(i, j, k) * rhowL;
+        iF(i, j, k, 0) = 0.5 * (CjR + CjL);
 
         // x momentum rho*u*Ui+ p*Ax
-        b.iF(i, j, k, 1) = 0.5 * ((rhouR * uR + pR) + (rhouL * uL + pL)) * a;
+        iF(i, j, k, 1) = 0.5 * (rhouR * UfR + pR * isx(i, j, k) + rhouL * UfL +
+                                pL * isx(i, j, k));
 
         // y momentum rho*v*Ui+ p*Ay
-        b.iF(i, j, k, 2) = 0.0;
+        iF(i, j, k, 2) = 0.5 * (rhovR * UfR + pR * isy(i, j, k) + rhovL * UfL +
+                                pL * isy(i, j, k));
 
         // w momentum rho*w*Ui+ p*Az
-        b.iF(i, j, k, 3) = 0.0;
+        iF(i, j, k, 3) = 0.5 * (rhowR * UfR + pR * isz(i, j, k) + rhowL * UfL +
+                                pL * isz(i, j, k));
 
         // Total energy (rhoE+ p)*Ui)
         double rhoER = b.Q(i, j, k, 4);
-        double rhoEL = b.Q(i - 1, j, k, 4);
+        double rhoEL = b.Q(i - iMod, j - jMod, k - kMod, 4);
 
-        b.iF(i, j, k, 4) = 0.5 * ((rhoER + pR) * uR + (rhoEL + pL) * uL) * a;
-
-        // Species
-        for (int n = 0; n < b.ne - 5; n++) {
-          b.iF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) * uR + b.Q(i - 1, j, k, 5 + n) * uL) *
-              a;
-        }
-      });
-
-  //-------------------------------------------------------------------------------------------|
-  // j flux face range
-  //-------------------------------------------------------------------------------------------|
-  MDRange3 range_j({b.ng, b.ng, b.ng},
-                   {b.ni + b.ng - 1, b.nj + b.ng, b.nk + b.ng - 1});
-  Kokkos::parallel_for(
-      "2nd order KEEP j face conv fluxes", range_j,
-      KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double V;
-        double uf, rhouf;
-        double vf, rhovf;
-        double wf, rhowf;
-        double pf;
-
-        // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j - 1, k, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j - 1, k, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j - 1, k, 3));
-
-        V = b.jsx(i, j, k) * uf + b.jsy(i, j, k) * vf + b.jsz(i, j, k) * wf;
-
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j - 1, k, 0));
-
-        rhouf = 0.5 * (b.Q(i, j, k, 1) + b.Q(i, j - 1, k, 1));
-        rhovf = 0.5 * (b.Q(i, j, k, 2) + b.Q(i, j - 1, k, 2));
-        rhowf = 0.5 * (b.Q(i, j, k, 3) + b.Q(i, j - 1, k, 3));
-
-        // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j - 1, k, 0));
-
-        // Continuity rho*Vj
-        b.jF(i, j, k, 0) = rho * V;
-
-        // x momentum rho*u*Vj+ pAx
-        b.jF(i, j, k, 1) = rhouf * V + pf * b.jsx(i, j, k);
-
-        // y momentum rho*v*Vj+ pAy
-        b.jF(i, j, k, 2) = rhovf * V + pf * b.jsy(i, j, k);
-
-        // w momentum rho*w*Vj+ pAz
-        b.jF(i, j, k, 3) = rhowf * V + pf * b.jsz(i, j, k);
-
-        // Total energy (rhoE+P)*Vj)
-        double rhoE = 0.5 * (b.Q(i, j, k, 4) + b.Q(i, j - 1, k, 4));
-
-        b.jF(i, j, k, 4) = (rhoE + pf) * V;
+        iF(i, j, k, 4) = 0.5 * ((rhoER + pR) * UfR + (rhoEL + pL) * UfL);
 
         // Species
         for (int n = 0; n < b.ne - 5; n++) {
-          b.jF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i, j - 1, k, 5 + n)) * V;
-        }
-      });
-
-  //-------------------------------------------------------------------------------------------|
-  // k flux face range
-  //-------------------------------------------------------------------------------------------|
-  MDRange3 range_k({b.ng, b.ng, b.ng},
-                   {b.ni + b.ng - 1, b.nj + b.ng - 1, b.nk + b.ng});
-  Kokkos::parallel_for(
-      "2nd order KEEP k face conv fluxes", range_k,
-      KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double W;
-        double uf, rhouf;
-        double vf, rhovf;
-        double wf, rhowf;
-        double pf;
-
-        // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j, k - 1, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j, k - 1, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j, k - 1, 3));
-
-        W = b.ksx(i, j, k) * uf + b.ksy(i, j, k) * vf + b.ksz(i, j, k) * wf;
-
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j, k - 1, 0));
-
-        rhouf = 0.5 * (b.Q(i, j, k, 1) + b.Q(i, j, k - 1, 1));
-        rhovf = 0.5 * (b.Q(i, j, k, 2) + b.Q(i, j, k - 1, 2));
-        rhowf = 0.5 * (b.Q(i, j, k, 3) + b.Q(i, j, k - 1, 3));
-
-        // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j, k - 1, 0));
-        // Continuity rho*Wk
-        b.kF(i, j, k, 0) = rho * W;
-
-        // x momentum rho*u*Wk+ pAx
-        b.kF(i, j, k, 1) = rhouf * W + pf * b.ksx(i, j, k);
-
-        // y momentum rho*v*Wk+ pAy
-        b.kF(i, j, k, 2) = rhovf * W + pf * b.ksy(i, j, k);
-
-        // w momentum rho*w*Wk+ pAz
-        b.kF(i, j, k, 3) = rhowf * W + pf * b.ksz(i, j, k);
-
-        // Total energy (rhoE+P)*Wk)
-        double rhoE = 0.5 * (b.Q(i, j, k, 4) + b.Q(i, j, k - 1, 4));
-
-        b.kF(i, j, k, 4) = (rhoE + pf) * W;
-
-        // Species
-        for (int n = 0; n < b.ne - 5; n++) {
-          b.kF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i, j, k - 1, 5 + n)) * W;
+          iF(i, j, k, 5 + n) =
+              0.5 * (b.Q(i, j, k, 5 + n) * UfR +
+                     b.Q(i - iMod, j - jMod, k - kMod, 5 + n) * UfL);
         }
       });
 }
+
+void centralDifference(block_ &b) {
+  computeFlux(b, b.iF, b.isx, b.isy, b.isz, 1, 0, 0);
+  computeFlux(b, b.jF, b.jsx, b.jsy, b.jsz, 0, 1, 0);
+  computeFlux(b, b.kF, b.ksx, b.ksy, b.ksz, 0, 0, 1);
+};
--- src/compute/advFlux/myKEEP.cpp
+++ src/compute/advFlux/myKEEP.cpp
@@ -1,63 +1,66 @@
-#include "Kokkos_Core.hpp"
 #include "block_.hpp"
 #include "kokkosTypes.hpp"
 #include "thtrdat_.hpp"
+#include <Kokkos_Core.hpp>
 
-void myKEEP(block_ &b) {
+static void computeFlux(const block_ &b, fourDview &iF, const threeDview &isx,
+                        const threeDview &isy, const threeDview &isz,
+                        const int iMod, const int jMod, const int kMod) {
 
-  //-------------------------------------------------------------------------------------------|
-  // i flux face range
-  //-------------------------------------------------------------------------------------------|
-  MDRange3 range_i({b.ng, b.ng, b.ng},
-                   {b.ni + b.ng, b.nj + b.ng - 1, b.nk + b.ng - 1});
+  // face flux range
+  MDRange3 range(
+      {b.ng, b.ng, b.ng},
+      {b.ni + b.ng - 1 + iMod, b.nj + b.ng - 1 + jMod, b.nk + b.ng - 1 + kMod});
+
   Kokkos::parallel_for(
-      "2nd order KEEP i face conv fluxes", range_i,
+      "2nd order myKEEP face conv fluxes", range,
       KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double U;
-        double uf;
-        double vf;
-        double wf;
-        double pf;
-
         // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i - 1, j, k, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i - 1, j, k, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i - 1, j, k, 3));
+        double uf =
+            0.5 * (b.q(i, j, k, 1) + b.q(i - iMod, j - jMod, k - kMod, 1));
+        double vf =
+            0.5 * (b.q(i, j, k, 2) + b.q(i - iMod, j - jMod, k - kMod, 2));
+        double wf =
+            0.5 * (b.q(i, j, k, 3) + b.q(i - iMod, j - jMod, k - kMod, 3));
 
-        U = b.isx(i, j, k) * uf + b.isy(i, j, k) * vf + b.isz(i, j, k) * wf;
+        double U = isx(i, j, k) * uf + isy(i, j, k) * vf + isz(i, j, k) * wf;
 
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i - 1, j, k, 0));
+        double pf =
+            0.5 * (b.q(i, j, k, 0) + b.q(i - iMod, j - jMod, k - kMod, 0));
 
         // Compute fluxes
         double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i - 1, j, k, 0));
+        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i - iMod, j - jMod, k - kMod, 0));
 
         // Continuity rho*Ui
-        b.iF(i, j, k, 0) = rho * U;
+        double Cj = rho * U;
+        iF(i, j, k, 0) = Cj;
 
         // x momentum rho*u*Ui+ p*Ax
-        b.iF(i, j, k, 1) = rho * uf * U + pf * b.isx(i, j, k);
+        iF(i, j, k, 1) = Cj * uf + pf * isx(i, j, k);
 
         // y momentum rho*v*Ui+ p*Ay
-        b.iF(i, j, k, 2) = rho * vf * U + pf * b.isy(i, j, k);
+        iF(i, j, k, 2) = Cj * vf + pf * isy(i, j, k);
 
         // w momentum rho*w*Ui+ p*Az
-        b.iF(i, j, k, 3) = rho * wf * U + pf * b.isz(i, j, k);
+        iF(i, j, k, 3) = Cj * wf + pf * isz(i, j, k);
 
         // Total energy (rhoE+ p)*Ui)
         double Kj = rho * 0.5 *
-                    (b.q(i, j, k, 1) * b.q(i - 1, j, k, 1) +
-                     b.q(i, j, k, 2) * b.q(i - 1, j, k, 2) +
-                     b.q(i, j, k, 3) * b.q(i - 1, j, k, 3)) *
+                    (b.q(i, j, k, 1) * b.q(i - iMod, j - jMod, k - kMod, 1) +
+                     b.q(i, j, k, 2) * b.q(i - iMod, j - jMod, k - kMod, 2) +
+                     b.q(i, j, k, 3) * b.q(i - iMod, j - jMod, k - kMod, 3)) *
                     U;
 
         double Pj =
-            0.5 * (b.q(i - 1, j, k, 0) * (b.q(i, j, k, 1) * b.isx(i, j, k) +
-                                          b.q(i, j, k, 2) * b.isy(i, j, k) +
-                                          b.q(i, j, k, 3) * b.isz(i, j, k)) +
-                   b.q(i, j, k, 0) * (b.q(i - 1, j, k, 1) * b.isx(i, j, k) +
-                                      b.q(i - 1, j, k, 2) * b.isy(i, j, k) +
-                                      b.q(i - 1, j, k, 3) * b.isz(i, j, k)));
+            0.5 * (b.q(i - iMod, j - jMod, k - kMod, 0) *
+                       (b.q(i, j, k, 1) * isx(i, j, k) +
+                        b.q(i, j, k, 2) * isy(i, j, k) +
+                        b.q(i, j, k, 3) * isz(i, j, k)) +
+                   b.q(i, j, k, 0) *
+                       (b.q(i - iMod, j - jMod, k - kMod, 1) * isx(i, j, k) +
+                        b.q(i - iMod, j - jMod, k - kMod, 2) * isy(i, j, k) +
+                        b.q(i - iMod, j - jMod, k - kMod, 3) * isz(i, j, k)));
 
         // solve for internal energy flux
         double cvR = b.qh(i, j, k, 1) / b.qh(i, j, k, 0);
@@ -72,7 +75,7 @@ void myKEEP(block_ &b) {
         double sR = cvR * log(TR) - RR * log(rhoR);
         double phiR =
             -RR * rhoR *
-            (uR * b.isx(i, j, k) + vR * b.isy(i, j, k) + wR * b.isz(i, j, k));
+            (uR * isx(i, j, k) + vR * isy(i, j, k) + wR * isz(i, j, k));
 
         double v0R =
             sR + (-hR + 0.5 * (pow(uR, 2) + pow(vR, 2) + pow(wR, 2))) / TR;
@@ -82,19 +85,20 @@ void myKEEP(block_ &b) {
         double v4R = 1.0 / TR;
 
         // left
-        double cvL = b.qh(i - 1, j, k, 1) / b.qh(i - 1, j, k, 0);
-        double &TL = b.q(i - 1, j, k, 4);
-        double &rhoL = b.Q(i - 1, j, k, 0);
-        double RL = b.qh(i - 1, j, k, 1) - cvL;
-        double hL = b.qh(i - 1, j, k, 2) / rhoL;
-        double eL = b.qh(i - 1, j, k, 4);
-        double &uL = b.q(i - 1, j, k, 1);
-        double &vL = b.q(i - 1, j, k, 2);
-        double &wL = b.q(i - 1, j, k, 3);
+        double cvL = b.qh(i - iMod, j - jMod, k - kMod, 1) /
+                     b.qh(i - iMod, j - jMod, k - kMod, 0);
+        double &TL = b.q(i - iMod, j - jMod, k - kMod, 4);
+        double &rhoL = b.Q(i - iMod, j - jMod, k - kMod, 0);
+        double RL = b.qh(i - iMod, j - jMod, k - kMod, 1) - cvL;
+        double hL = b.qh(i - iMod, j - jMod, k - kMod, 2) / rhoL;
+        double eL = b.qh(i - iMod, j - jMod, k - kMod, 4);
+        double &uL = b.q(i - iMod, j - jMod, k - kMod, 1);
+        double &vL = b.q(i - iMod, j - jMod, k - kMod, 2);
+        double &wL = b.q(i - iMod, j - jMod, k - kMod, 3);
         double sL = cvL * log(TL) - RL * log(rhoL);
         double phiL =
             -RL * rhoL *
-            (uL * b.isx(i, j, k) + vL * b.isy(i, j, k) + wL * b.isz(i, j, k));
+            (uL * isx(i, j, k) + vL * isy(i, j, k) + wL * isz(i, j, k));
 
         double v0L =
             sL + (-hL + 0.5 * (pow(uL, 2) + pow(vL, 2) + pow(wL, 2))) / TL;
@@ -121,7 +125,7 @@ void myKEEP(block_ &b) {
         //
         // Whereas if we use either the quadratic or divergent forms
         // double Fs = 0.5 * (rhoR * sR + rhoL * sL) * U;
-        // double Fs = 0.5 * (rhoR * uR * sR + rhoL * uL * sL) * b.iS(i, j, k);
+        // double Fs = 0.5 * (rhoR * uR * sR + rhoL * uL * sL) * iS(i, j, k);
         //
         // Then we are substantially different from the KEEPep scheme. In fact
         // the original KEEP scheme, none of the forms of Fs results in the same
@@ -129,294 +133,26 @@ void myKEEP(block_ &b) {
         // We match exactly with v.\delF/delx evolution, where as for the cubic
         // form of Fs, we do not match v.\delF/\delx. wtf.
 
-        double Ij = (Fs + PHI - V0 * b.iF(i, j, k, 0) - V1 * b.iF(i, j, k, 1) -
-                     V2 * b.iF(i, j, k, 2) - V3 * b.iF(i, j, k, 3)) /
+        double Ij = (Fs + PHI - V0 * iF(i, j, k, 0) - V1 * iF(i, j, k, 1) -
+                     V2 * iF(i, j, k, 2) - V3 * iF(i, j, k, 3)) /
                         V4 -
                     Pj - Kj;
         Ij = (eL * TR + eR * TL) / (TL + TR) * U;
 
-        b.iF(i, j, k, 4) = Ij + Kj + Pj;
+        iF(i, j, k, 4) = Ij + Kj + Pj;
 
         // Species
         for (int n = 0; n < b.ne - 5; n++) {
-          b.iF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i - 1, j, k, 5 + n)) * U;
+          iF(i, j, k, 5 + n) =
+              0.5 *
+              (b.Q(i, j, k, 5 + n) + b.Q(i - iMod, j - jMod, k - kMod, 5 + n)) *
+              U;
         }
       });
+}
 
-  //-------------------------------------------------------------------------------------------|
-  // j flux face range
-  //-------------------------------------------------------------------------------------------|
-  MDRange3 range_j({b.ng, b.ng, b.ng},
-                   {b.ni + b.ng - 1, b.nj + b.ng, b.nk + b.ng - 1});
-  Kokkos::parallel_for(
-      "2nd order KEEP j face conv fluxes", range_j,
-      KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double V;
-        double uf;
-        double vf;
-        double wf;
-        double pf;
-
-        // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j - 1, k, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j - 1, k, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j - 1, k, 3));
-
-        V = b.jsx(i, j, k) * uf + b.jsy(i, j, k) * vf + b.jsz(i, j, k) * wf;
-
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j - 1, k, 0));
-
-        // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j - 1, k, 0));
-
-        // Continuity rho*Vj
-        b.jF(i, j, k, 0) = rho * V;
-
-        // x momentum rho*u*Vj+ pAx
-        b.jF(i, j, k, 1) = rho * uf * V + pf * b.jsx(i, j, k);
-
-        // y momentum rho*v*Vj+ pAy
-        b.jF(i, j, k, 2) = rho * vf * V + pf * b.jsy(i, j, k);
-
-        // w momentum rho*w*Vj+ pAz
-        b.jF(i, j, k, 3) = rho * wf * V + pf * b.jsz(i, j, k);
-
-        // Total energy (rhoE+P)*Vj)
-        double Kj = rho * 0.5 *
-                    (b.q(i, j, k, 1) * b.q(i, j - 1, k, 1) +
-                     b.q(i, j, k, 2) * b.q(i, j - 1, k, 2) +
-                     b.q(i, j, k, 3) * b.q(i, j - 1, k, 3)) *
-                    V;
-
-        double Pj =
-            0.5 * (b.q(i, j - 1, k, 0) * (b.q(i, j, k, 1) * b.jsx(i, j, k) +
-                                          b.q(i, j, k, 2) * b.jsy(i, j, k) +
-                                          b.q(i, j, k, 3) * b.jsz(i, j, k)) +
-                   b.q(i, j, k, 0) * (b.q(i, j - 1, k, 1) * b.jsx(i, j, k) +
-                                      b.q(i, j - 1, k, 2) * b.jsy(i, j, k) +
-                                      b.q(i, j - 1, k, 3) * b.jsz(i, j, k)));
-
-        // solve for internal energy flux
-        double cvR = b.qh(i, j, k, 1) / b.qh(i, j, k, 0);
-        double &TR = b.q(i, j, k, 4);
-        double &rhoR = b.Q(i, j, k, 0);
-        double RR = b.qh(i, j, k, 1) - cvR;
-        double hR = b.qh(i, j, k, 2) / rhoR;
-        double eR = b.qh(i, j, k, 4);
-        double &uR = b.q(i, j, k, 1);
-        double &vR = b.q(i, j, k, 2);
-        double &wR = b.q(i, j, k, 3);
-        double sR = cvR * log(TR) - RR * log(rhoR);
-        double phiR =
-            -RR * rhoR *
-            (uR * b.jsx(i, j, k) + vR * b.jsy(i, j, k) + wR * b.jsz(i, j, k));
-
-        double v0R =
-            sR + (-hR + 0.5 * (pow(uR, 2) + pow(vR, 2) + pow(wR, 2))) / TR;
-        double v1R = -uR / TR;
-        double v2R = -vR / TR;
-        double v3R = -wR / TR;
-        double v4R = 1.0 / TR;
-
-        // left
-        double cvL = b.qh(i, j - 1, k, 1) / b.qh(i, j - 1, k, 0);
-        double &TL = b.q(i, j - 1, k, 4);
-        double &rhoL = b.Q(i, j - 1, k, 0);
-        double RL = b.qh(i, j - 1, k, 1) - cvL;
-        double hL = b.qh(i, j - 1, k, 2) / rhoL;
-        double eL = b.qh(i, j - 1, k, 4);
-        double &uL = b.q(i, j - 1, k, 1);
-        double &vL = b.q(i, j - 1, k, 2);
-        double &wL = b.q(i, j - 1, k, 3);
-        double sL = cvL * log(TL) - RL * log(rhoL);
-        double phiL =
-            -RL * rhoL *
-            (uL * b.jsx(i, j, k) + vL * b.jsy(i, j, k) + wL * b.jsz(i, j, k));
-
-        double v0L =
-            sL + (-hL + 0.5 * (pow(uL, 2) + pow(vL, 2) + pow(wL, 2))) / TL;
-        double v1L = -uL / TL;
-        double v2L = -vL / TL;
-        double v3L = -wL / TL;
-        double v4L = 1.0 / TL;
-
-        double V0 = 0.5 * (v0R + v0L);
-        double V1 = 0.5 * (v1R + v1L);
-        double V2 = 0.5 * (v2R + v2L);
-        double V3 = 0.5 * (v3R + v3L);
-        double V4 = 0.5 * (v4R + v4L);
-        double PHI = 0.5 * (phiR + phiL);
-
-        // This comes from Eq. 4.5c of Tamdor, but there are consequences to the
-        // form of G_v+1/2 (Fs_m+1/2)
-        // If we use the cubic form
-        //
-        double Fs = rho * V * 0.5 * (sR + sL);
-        //
-        // Then we actually match the KEEPep scheme almost float for float,
-        // so close that there has to be an equality between them.
-        //
-        // Whereas if we use either the quadratic or divergent forms
-        // double Fs = 0.5 * (rhoR * sR + rhoL * sL) * V;
-        //
-        // Then we are substantially different from the KEEPep scheme. In fact
-        // the original KEEP scheme, none of the forms of Fs results in the same
-        // answer. However, when we use the quadratic and divergent forms
-        // We match exactly with v.\delF/delx evolution, where as for the cubic
-        // form of Fs, we do not match v.\delF/\delx. wtf.
-
-        double Ij = (Fs + PHI - V0 * b.jF(i, j, k, 0) - V1 * b.jF(i, j, k, 1) -
-                     V2 * b.jF(i, j, k, 2) - V3 * b.jF(i, j, k, 3)) /
-                        V4 -
-                    Pj - Kj;
-
-        Ij = (eL * TR + eR * TL) / (TL + TR) * V;
-
-        b.jF(i, j, k, 4) = Ij + Kj + Pj;
-
-        // Species
-        for (int n = 0; n < b.ne - 5; n++) {
-          b.jF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i, j - 1, k, 5 + n)) * V;
-        }
-      });
-
-  //-------------------------------------------------------------------------------------------|
-  // k flux face range
-  //-------------------------------------------------------------------------------------------|
-  MDRange3 range_k({b.ng, b.ng, b.ng},
-                   {b.ni + b.ng - 1, b.nj + b.ng - 1, b.nk + b.ng});
-  Kokkos::parallel_for(
-      "2nd order KEEP k face conv fluxes", range_k,
-      KOKKOS_LAMBDA(const int i, const int j, const int k) {
-        double W;
-        double uf;
-        double vf;
-        double wf;
-        double pf;
-
-        // Compute face normal volume flux vector
-        uf = 0.5 * (b.q(i, j, k, 1) + b.q(i, j, k - 1, 1));
-        vf = 0.5 * (b.q(i, j, k, 2) + b.q(i, j, k - 1, 2));
-        wf = 0.5 * (b.q(i, j, k, 3) + b.q(i, j, k - 1, 3));
-
-        W = b.ksx(i, j, k) * uf + b.ksy(i, j, k) * vf + b.ksz(i, j, k) * wf;
-
-        pf = 0.5 * (b.q(i, j, k, 0) + b.q(i, j, k - 1, 0));
-
-        // Compute fluxes
-        double rho;
-        rho = 0.5 * (b.Q(i, j, k, 0) + b.Q(i, j, k - 1, 0));
-        // Continuity rho*Wk
-        b.kF(i, j, k, 0) = rho * W;
-
-        // x momentum rho*u*Wk+ pAx
-        b.kF(i, j, k, 1) = rho * uf * W + pf * b.ksx(i, j, k);
-
-        // y momentum rho*v*Wk+ pAy
-        b.kF(i, j, k, 2) = rho * vf * W + pf * b.ksy(i, j, k);
-
-        // w momentum rho*w*Wk+ pAz
-        b.kF(i, j, k, 3) = rho * wf * W + pf * b.ksz(i, j, k);
-
-        // Total energy (rhoE+P)*Wk)
-        double Kj = rho * 0.5 *
-                    (b.q(i, j, k, 1) * b.q(i, j, k - 1, 1) +
-                     b.q(i, j, k, 2) * b.q(i, j, k - 1, 2) +
-                     b.q(i, j, k, 3) * b.q(i, j, k - 1, 3)) *
-                    W;
-
-        double Pj =
-            0.5 * (b.q(i, j, k - 1, 0) * (b.q(i, j, k, 1) * b.ksx(i, j, k) +
-                                          b.q(i, j, k, 2) * b.ksy(i, j, k) +
-                                          b.q(i, j, k, 3) * b.ksz(i, j, k)) +
-                   b.q(i, j, k, 0) * (b.q(i, j, k - 1, 1) * b.ksx(i, j, k) +
-                                      b.q(i, j, k - 1, 2) * b.ksy(i, j, k) +
-                                      b.q(i, j, k - 1, 3) * b.ksz(i, j, k)));
-
-        // solve for internal energy flux
-        double cvR = b.qh(i, j, k, 1) / b.qh(i, j, k, 0);
-        double &TR = b.q(i, j, k, 4);
-        double &rhoR = b.Q(i, j, k, 0);
-        double RR = b.qh(i, j, k, 1) - cvR;
-        double hR = b.qh(i, j, k, 2) / rhoR;
-        double eR = b.qh(i, j, k, 4);
-        double &uR = b.q(i, j, k, 1);
-        double &vR = b.q(i, j, k, 2);
-        double &wR = b.q(i, j, k, 3);
-        double sR = cvR * log(TR) - RR * log(rhoR);
-        double phiR =
-            -RR * rhoR *
-            (uR * b.ksx(i, j, k) + vR * b.ksy(i, j, k) + wR * b.ksz(i, j, k));
-
-        double v0R =
-            sR + (-hR + 0.5 * (pow(uR, 2) + pow(vR, 2) + pow(wR, 2))) / TR;
-        double v1R = -uR / TR;
-        double v2R = -vR / TR;
-        double v3R = -wR / TR;
-        double v4R = 1.0 / TR;
-
-        // left
-        double cvL = b.qh(i, j, k - 1, 1) / b.qh(i, j, k - 1, 0);
-        double &TL = b.q(i, j, k - 1, 4);
-        double &rhoL = b.Q(i, j, k - 1, 0);
-        double RL = b.qh(i, j, k - 1, 1) - cvL;
-        double hL = b.qh(i, j, k - 1, 2) / rhoL;
-        double eL = b.qh(i, j, k - 1, 4);
-        double &uL = b.q(i, j, k - 1, 1);
-        double &vL = b.q(i, j, k - 1, 2);
-        double &wL = b.q(i, j, k - 1, 3);
-        double sL = cvL * log(TL) - RL * log(rhoL);
-        double phiL =
-            -RL * rhoL *
-            (uL * b.ksx(i, j, k) + vL * b.ksy(i, j, k) + wL * b.ksz(i, j, k));
-
-        double v0L =
-            sL + (-hL + 0.5 * (pow(uL, 2) + pow(vL, 2) + pow(wL, 2))) / TL;
-        double v1L = -uL / TL;
-        double v2L = -vL / TL;
-        double v3L = -wL / TL;
-        double v4L = 1.0 / TL;
-
-        double V0 = 0.5 * (v0R + v0L);
-        double V1 = 0.5 * (v1R + v1L);
-        double V2 = 0.5 * (v2R + v2L);
-        double V3 = 0.5 * (v3R + v3L);
-        double V4 = 0.5 * (v4R + v4L);
-        double PHI = 0.5 * (phiR + phiL);
-
-        // This comes from Eq. 4.5c of Tamdor, but there are consequences to the
-        // form of G_v+1/2 (Fs_m+1/2)
-        // If we use the cubic form
-        //
-        double Fs = rho * W * 0.5 * (sR + sL);
-        //
-        // Then we actually match the KEEPep scheme almost float for float,
-        // so close that there has to be an equality between them.
-        //
-        // Whereas if we use either the quadratic or divergent forms
-        // double Fs = 0.5 * (rhoR * sR + rhoL * sL) * W;
-        //
-        // Then we are substantially different from the KEEPep scheme. In fact
-        // the original KEEP scheme, none of the forms of Fs results in the same
-        // answer. However, when we use the quadratic and divergent forms
-        // We match exactly with v.\delF/delx evolution, where as for the cubic
-        // form of Fs, we do not match v.\delF/\delx. wtf.
-
-        double Ij = (Fs + PHI - V0 * b.kF(i, j, k, 0) - V1 * b.kF(i, j, k, 1) -
-                     V2 * b.kF(i, j, k, 2) - V3 * b.kF(i, j, k, 3)) /
-                        V4 -
-                    Pj - Kj;
-
-        Ij = (eL * TR + eR * TL) / (TL + TR) * W;
-
-        b.kF(i, j, k, 4) = Ij + Kj + Pj;
-        // Species
-        for (int n = 0; n < b.ne - 5; n++) {
-          b.kF(i, j, k, 5 + n) =
-              0.5 * (b.Q(i, j, k, 5 + n) + b.Q(i, j, k - 1, 5 + n)) * W;
-        }
-      });
+void myKEEP(block_ &b) {
+  computeFlux(b, b.iF, b.isx, b.isy, b.isz, 1, 0, 0);
+  computeFlux(b, b.jF, b.jsx, b.jsy, b.jsz, 0, 1, 0);
+  computeFlux(b, b.kF, b.ksx, b.ksy, b.ksz, 0, 0, 1);
 }
